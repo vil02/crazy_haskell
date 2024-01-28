f23752abc85c9e86154f544aafcbed893f148903
vil02 <vil02@o2.pl>
2024-01-28 14:39:25 +0100

    fix: simplify `generate_output.sh`

--- generate_output.sh
+++ generate_output.sh
@@ -2,10 +2,6 @@
 
 set -euo pipefail
 
-declare current_time
-current_time=$(date "+%d.%m.%Y %H:%M:%S.%N")
-readonly current_time
-
 if [[ $# -eq 0 ]]; then
   echo "Usage: $0 filename"
   exit 1
@@ -20,5 +16,4 @@ if [[ -e "${output_file}" ]]; then
 fi
 
 declare -ri length=$((RANDOM % 1000 + 1))
-echo "${current_time}" >> "${output_file}"
 tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w "${length}" | head -n 1 >> "${output_file}"
