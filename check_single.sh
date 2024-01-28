43723edd28f0b55ffcbf765716af57b0c4a9c547
vil02 <vil02@o2.pl>
2024-01-28 12:07:56 +0100

    chore: add `.gitignore`

--- check_single.sh
+++ check_single.sh
@@ -8,17 +8,17 @@ fi
 
 declare -r core_name=$1
 
-if ! ghc -o "${core_name}" "${core_name}.hs" &> /dev/null; then
+if ! ghc -o "${core_name}.out" "${core_name}.hs" &> /dev/null; then
     echo "${core_name}: Compilation failed."
     exit 2
 fi
 
 declare -i result_code=0
-if ! diff <(./"${core_name}") "${core_name}.txt" &> /dev/null; then
+if ! diff <(./"${core_name}.out") "${core_name}.txt" &> /dev/null; then
     echo "${core_name}: Wrong output."
     result_code=3
 fi
 
-rm "${core_name}" "${core_name}.o" "${core_name}.hi"
+rm "${core_name}.out" "${core_name}.o" "${core_name}.hi"
 
 exit "${result_code}"
