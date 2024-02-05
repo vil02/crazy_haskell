befcd339ec28a0dac1bbe6d2a4fb878b64799319
vil02 <vil02@users.noreply.github.com>
2024-02-05 11:46:39 +0000

    feat: add `p_32`

---
+++ src/p_32.hs
@@ -0,0 +1,39 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_11
+
+fun_0 :: String
+fun_0 = "5" ++ "l"
+
+fun_1 :: String
+fun_1 = "n" ++ "L" ++ fun_0
+
+fun_2 :: String
+fun_2 = "m" ++ "l"
+
+fun_3 :: String
+fun_3 = "q" ++ "T"
+
+fun_4 :: String
+fun_4 = "n" ++ "k" ++ "Z"
+
+fun_5 :: String
+fun_5 = "y" ++ fun_4 ++ "f"
+
+fun_6 :: String
+fun_6 = fun_3 ++ fun_5
+
+fun_7 :: String
+fun_7 = "M" ++ "J"
+
+fun_8 :: String
+fun_8 = "b" ++ fun_7
+
+fun_9 :: String
+fun_9 = "Z" ++ fun_8
+
+fun_10 :: String
+fun_10 = fun_6 ++ "y" ++ fun_9
+
+fun_11 :: String
+fun_11 = "S" ++ fun_1 ++ fun_2 ++ fun_10
