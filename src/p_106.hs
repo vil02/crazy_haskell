47c2ecc02a176d264c2e1f830ba1fddc18012779
vil02 <vil02@users.noreply.github.com>
2024-03-01 01:39:58 +0000

    feat: add `p_106`

---
+++ src/p_106.hs
@@ -0,0 +1,9 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_1
+
+fun_0 :: String
+fun_0 = "v" ++ "A" ++ "z"
+
+fun_1 :: String
+fun_1 = "x" ++ fun_0 ++ "Q" ++ "a" ++ "C"
