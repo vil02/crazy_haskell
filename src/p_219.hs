7458f18678d927b095fcb00b01e5114778d55331
vil02 <vil02@users.noreply.github.com>
2024-04-11 22:46:49 +0000

    feat: add `p_219`

---
+++ src/p_219.hs
@@ -0,0 +1,51 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_15
+
+fun_0 :: String
+fun_0 = "k" ++ "O"
+
+fun_1 :: String
+fun_1 = fun_0 ++ "3"
+
+fun_2 :: String
+fun_2 = "1" ++ "r"
+
+fun_3 :: String
+fun_3 = "T" ++ "L"
+
+fun_4 :: String
+fun_4 = fun_3 ++ "q"
+
+fun_5 :: String
+fun_5 = "y" ++ "v"
+
+fun_6 :: String
+fun_6 = fun_5 ++ "i"
+
+fun_7 :: String
+fun_7 = "2" ++ fun_6 ++ "p"
+
+fun_8 :: String
+fun_8 = "r" ++ "t" ++ fun_7
+
+fun_9 :: String
+fun_9 = fun_4 ++ fun_8
+
+fun_10 :: String
+fun_10 = fun_2 ++ fun_9
+
+fun_11 :: String
+fun_11 = "0" ++ fun_1 ++ fun_10
+
+fun_12 :: String
+fun_12 = "m" ++ "8"
+
+fun_13 :: String
+fun_13 = "h" ++ "j"
+
+fun_14 :: String
+fun_14 = fun_12 ++ fun_13
+
+fun_15 :: String
+fun_15 = fun_11 ++ fun_14 ++ "D" ++ "S"
