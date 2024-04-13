bc784cb4c2d711c19e6fbb0037bd25a3ea6c96b7
vil02 <vil02@users.noreply.github.com>
2024-04-13 11:47:02 +0000

    feat: add `p_224`

---
+++ src/p_224.hs
@@ -0,0 +1,54 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_16
+
+fun_0 :: String
+fun_0 = "j" ++ "3"
+
+fun_1 :: String
+fun_1 = "J" ++ fun_0 ++ "n" ++ "Y"
+
+fun_2 :: String
+fun_2 = "Y" ++ "p"
+
+fun_3 :: String
+fun_3 = "j" ++ "o"
+
+fun_4 :: String
+fun_4 = fun_3 ++ "Y"
+
+fun_5 :: String
+fun_5 = "5" ++ "e"
+
+fun_6 :: String
+fun_6 = fun_5 ++ "6" ++ "g"
+
+fun_7 :: String
+fun_7 = "T" ++ "N"
+
+fun_8 :: String
+fun_8 = "1" ++ fun_7
+
+fun_9 :: String
+fun_9 = fun_6 ++ fun_8
+
+fun_10 :: String
+fun_10 = "g" ++ "9"
+
+fun_11 :: String
+fun_11 = "z" ++ fun_1 ++ fun_2 ++ fun_4 ++ fun_9 ++ fun_10 ++ "t"
+
+fun_12 :: String
+fun_12 = "p" ++ "K"
+
+fun_13 :: String
+fun_13 = "9" ++ fun_12 ++ "T"
+
+fun_14 :: String
+fun_14 = fun_11 ++ "u" ++ fun_13
+
+fun_15 :: String
+fun_15 = "8" ++ "z"
+
+fun_16 :: String
+fun_16 = "D" ++ fun_14 ++ fun_15
