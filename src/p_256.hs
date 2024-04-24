a7374f6b9141a804e19c1270ffd0dbe791070d06
vil02 <vil02@users.noreply.github.com>
2024-04-24 01:39:39 +0000

    feat: add `p_256`

---
+++ src/p_256.hs
@@ -0,0 +1,27 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_7
+
+fun_0 :: String
+fun_0 = "n" ++ "C"
+
+fun_1 :: String
+fun_1 = "y" ++ "C"
+
+fun_2 :: String
+fun_2 = "u" ++ "8"
+
+fun_3 :: String
+fun_3 = "W" ++ "g"
+
+fun_4 :: String
+fun_4 = fun_1 ++ fun_2 ++ fun_3
+
+fun_5 :: String
+fun_5 = "y" ++ "O"
+
+fun_6 :: String
+fun_6 = fun_5 ++ "S"
+
+fun_7 :: String
+fun_7 = fun_0 ++ fun_4 ++ fun_6
