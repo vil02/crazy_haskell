17f9ffc80127e060399c5dc93a57ac2c055981de
vil02 <vil02@users.noreply.github.com>
2024-05-17 22:47:01 +0000

    feat: add `p_328`

---
+++ src/p_328.hs
@@ -0,0 +1,42 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_12
+
+fun_0 :: String
+fun_0 = "D" ++ "b"
+
+fun_1 :: String
+fun_1 = "x" ++ "x"
+
+fun_2 :: String
+fun_2 = "j" ++ fun_1 ++ "q"
+
+fun_3 :: String
+fun_3 = "t" ++ "n" ++ "Q"
+
+fun_4 :: String
+fun_4 = "2" ++ "3"
+
+fun_5 :: String
+fun_5 = "E" ++ fun_4
+
+fun_6 :: String
+fun_6 = fun_5 ++ "N"
+
+fun_7 :: String
+fun_7 = "I" ++ "6" ++ fun_3 ++ fun_6
+
+fun_8 :: String
+fun_8 = "T" ++ fun_7
+
+fun_9 :: String
+fun_9 = fun_0 ++ fun_2 ++ fun_8 ++ "5"
+
+fun_10 :: String
+fun_10 = "H" ++ "P"
+
+fun_11 :: String
+fun_11 = "I" ++ "4"
+
+fun_12 :: String
+fun_12 = "x" ++ fun_9 ++ fun_10 ++ "l" ++ fun_11 ++ "d"
