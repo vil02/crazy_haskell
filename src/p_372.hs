d208438453ecd529947b2bd7265d882c28dc2390
vil02 <vil02@users.noreply.github.com>
2024-06-01 11:46:56 +0000

    feat: add `p_372`

---
+++ src/p_372.hs
@@ -0,0 +1,33 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_9
+
+fun_0 :: String
+fun_0 = "S" ++ "7"
+
+fun_1 :: String
+fun_1 = fun_0 ++ "p"
+
+fun_2 :: String
+fun_2 = "5" ++ "J"
+
+fun_3 :: String
+fun_3 = "T" ++ "9"
+
+fun_4 :: String
+fun_4 = "O" ++ "2"
+
+fun_5 :: String
+fun_5 = fun_2 ++ "G" ++ fun_3 ++ fun_4 ++ "D"
+
+fun_6 :: String
+fun_6 = "y" ++ fun_1 ++ "9" ++ fun_5
+
+fun_7 :: String
+fun_7 = "a" ++ "N"
+
+fun_8 :: String
+fun_8 = "l" ++ "t"
+
+fun_9 :: String
+fun_9 = fun_6 ++ "N" ++ fun_7 ++ fun_8 ++ "H"
