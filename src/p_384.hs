e978efc1c49acdf35af742ad0c83812a57b568d0
vil02 <vil02@users.noreply.github.com>
2024-06-05 11:47:03 +0000

    feat: add `p_384`

---
+++ src/p_384.hs
@@ -0,0 +1,12 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_2
+
+fun_0 :: String
+fun_0 = "u" ++ "L"
+
+fun_1 :: String
+fun_1 = "Z" ++ "r"
+
+fun_2 :: String
+fun_2 = fun_0 ++ fun_1 ++ "S"
