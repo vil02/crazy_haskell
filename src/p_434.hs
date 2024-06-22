18156cca1d956063038f48751a46316ad291bf20
vil02 <vil02@users.noreply.github.com>
2024-06-22 01:43:16 +0000

    feat: add `p_434`

---
+++ src/p_434.hs
@@ -0,0 +1,36 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_10
+
+fun_0 :: String
+fun_0 = "4" ++ "t"
+
+fun_1 :: String
+fun_1 = "l" ++ "G"
+
+fun_2 :: String
+fun_2 = fun_1 ++ "G"
+
+fun_3 :: String
+fun_3 = "E" ++ fun_2
+
+fun_4 :: String
+fun_4 = "f" ++ fun_3 ++ "J" ++ "v"
+
+fun_5 :: String
+fun_5 = "0" ++ "I"
+
+fun_6 :: String
+fun_6 = fun_4 ++ fun_5
+
+fun_7 :: String
+fun_7 = "d" ++ "D"
+
+fun_8 :: String
+fun_8 = "E" ++ fun_7
+
+fun_9 :: String
+fun_9 = "j" ++ fun_8 ++ "l" ++ "d"
+
+fun_10 :: String
+fun_10 = fun_0 ++ "e" ++ fun_6 ++ "h" ++ fun_9 ++ "H"
