83da5095ea39b08a5c6c0a6c284f74c22e3660ad
vil02 <vil02@users.noreply.github.com>
2024-06-24 01:47:24 +0000

    feat: add `p_440`

---
+++ src/p_440.hs
@@ -0,0 +1,12 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_2
+
+fun_0 :: String
+fun_0 = "Q" ++ "d"
+
+fun_1 :: String
+fun_1 = fun_0 ++ "y"
+
+fun_2 :: String
+fun_2 = "r" ++ fun_1 ++ "D"
