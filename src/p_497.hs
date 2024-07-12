5a949e4d80ed08e02d83e0964d52b6754ba2110e
vil02 <vil02@users.noreply.github.com>
2024-07-12 22:46:54 +0000

    feat: add `p_497`

---
+++ src/p_497.hs
@@ -0,0 +1,54 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_16
+
+fun_0 :: String
+fun_0 = "F" ++ "6"
+
+fun_1 :: String
+fun_1 = "C" ++ "s"
+
+fun_2 :: String
+fun_2 = "o" ++ "O"
+
+fun_3 :: String
+fun_3 = fun_1 ++ fun_2 ++ "l" ++ "p"
+
+fun_4 :: String
+fun_4 = fun_3 ++ "Y"
+
+fun_5 :: String
+fun_5 = "Q" ++ fun_4
+
+fun_6 :: String
+fun_6 = "W" ++ "J"
+
+fun_7 :: String
+fun_7 = "F" ++ fun_6 ++ "h"
+
+fun_8 :: String
+fun_8 = fun_7 ++ "9"
+
+fun_9 :: String
+fun_9 = "4" ++ "A"
+
+fun_10 :: String
+fun_10 = fun_5 ++ fun_8 ++ "J" ++ fun_9
+
+fun_11 :: String
+fun_11 = "l" ++ "c"
+
+fun_12 :: String
+fun_12 = "y" ++ "O"
+
+fun_13 :: String
+fun_13 = fun_11 ++ fun_12
+
+fun_14 :: String
+fun_14 = "P" ++ "r" ++ "H"
+
+fun_15 :: String
+fun_15 = fun_13 ++ fun_14
+
+fun_16 :: String
+fun_16 = fun_0 ++ fun_10 ++ "W" ++ "C" ++ fun_15 ++ "5" ++ "C"
