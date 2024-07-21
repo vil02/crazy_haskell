23ba60197930034a4517160044f11f63a13618a0
vil02 <vil02@users.noreply.github.com>
2024-07-21 01:55:20 +0000

    feat: add `p_523`

---
+++ src/p_523.hs
@@ -0,0 +1,60 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_18
+
+fun_0 :: String
+fun_0 = "o" ++ "y"
+
+fun_1 :: String
+fun_1 = "O" ++ "s"
+
+fun_2 :: String
+fun_2 = "i" ++ "n"
+
+fun_3 :: String
+fun_3 = fun_2 ++ "1"
+
+fun_4 :: String
+fun_4 = "O" ++ fun_3
+
+fun_5 :: String
+fun_5 = "B" ++ fun_4
+
+fun_6 :: String
+fun_6 = "D" ++ "Q"
+
+fun_7 :: String
+fun_7 = "m" ++ fun_6
+
+fun_8 :: String
+fun_8 = "m" ++ "2" ++ "H"
+
+fun_9 :: String
+fun_9 = "c" ++ "9"
+
+fun_10 :: String
+fun_10 = fun_9 ++ "Y"
+
+fun_11 :: String
+fun_11 = fun_8 ++ fun_10
+
+fun_12 :: String
+fun_12 = "v" ++ "k"
+
+fun_13 :: String
+fun_13 = "M" ++ "z"
+
+fun_14 :: String
+fun_14 = fun_12 ++ fun_13
+
+fun_15 :: String
+fun_15 = fun_11 ++ fun_14
+
+fun_16 :: String
+fun_16 = "V" ++ "v"
+
+fun_17 :: String
+fun_17 = fun_16 ++ "a"
+
+fun_18 :: String
+fun_18 = fun_0 ++ fun_1 ++ "a" ++ "c" ++ fun_5 ++ fun_7 ++ fun_15 ++ fun_17
