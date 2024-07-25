bc6432fd03bba7a3e5da023b321b4d42e216966b
vil02 <vil02@users.noreply.github.com>
2024-07-25 11:46:50 +0000

    feat: add `p_536`

---
+++ src/p_536.hs
@@ -0,0 +1,42 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_12
+
+fun_0 :: String
+fun_0 = "c" ++ "7"
+
+fun_1 :: String
+fun_1 = "i" ++ "M"
+
+fun_2 :: String
+fun_2 = "X" ++ "i"
+
+fun_3 :: String
+fun_3 = "Q" ++ "R"
+
+fun_4 :: String
+fun_4 = "B" ++ fun_3 ++ "g"
+
+fun_5 :: String
+fun_5 = fun_0 ++ fun_1 ++ fun_2 ++ "Q" ++ fun_4
+
+fun_6 :: String
+fun_6 = "a" ++ "u"
+
+fun_7 :: String
+fun_7 = "d" ++ "2" ++ "Q"
+
+fun_8 :: String
+fun_8 = "C" ++ "w"
+
+fun_9 :: String
+fun_9 = fun_7 ++ fun_8
+
+fun_10 :: String
+fun_10 = "1" ++ fun_6 ++ fun_9
+
+fun_11 :: String
+fun_11 = "b" ++ "Q"
+
+fun_12 :: String
+fun_12 = fun_5 ++ fun_10 ++ fun_11
