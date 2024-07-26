e7f3cb79bf591f0c766f65451207b963327a5f22
vil02 <vil02@users.noreply.github.com>
2024-07-26 11:46:40 +0000

    feat: add `p_539`

---
+++ src/p_539.hs
@@ -0,0 +1,45 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_13
+
+fun_0 :: String
+fun_0 = "8" ++ "p"
+
+fun_1 :: String
+fun_1 = "C" ++ fun_0
+
+fun_2 :: String
+fun_2 = fun_1 ++ "Q"
+
+fun_3 :: String
+fun_3 = "8" ++ fun_2
+
+fun_4 :: String
+fun_4 = "e" ++ "4"
+
+fun_5 :: String
+fun_5 = "9" ++ fun_4
+
+fun_6 :: String
+fun_6 = "b" ++ "e"
+
+fun_7 :: String
+fun_7 = "f" ++ "s" ++ "s" ++ "m"
+
+fun_8 :: String
+fun_8 = fun_7 ++ "s"
+
+fun_9 :: String
+fun_9 = "5" ++ "k"
+
+fun_10 :: String
+fun_10 = fun_8 ++ fun_9 ++ "V"
+
+fun_11 :: String
+fun_11 = "u" ++ "7" ++ "O"
+
+fun_12 :: String
+fun_12 = fun_6 ++ fun_10 ++ fun_11 ++ "t"
+
+fun_13 :: String
+fun_13 = "s" ++ fun_3 ++ fun_5 ++ "S" ++ fun_12 ++ "9"
