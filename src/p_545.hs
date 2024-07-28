8b1ef04693cdf51836019ec4ef14114ef2b23dd1
vil02 <vil02@users.noreply.github.com>
2024-07-28 11:46:34 +0000

    feat: add `p_545`

---
+++ src/p_545.hs
@@ -0,0 +1,21 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_5
+
+fun_0 :: String
+fun_0 = "5" ++ "e"
+
+fun_1 :: String
+fun_1 = fun_0 ++ "V"
+
+fun_2 :: String
+fun_2 = "0" ++ "3"
+
+fun_3 :: String
+fun_3 = fun_2 ++ "E"
+
+fun_4 :: String
+fun_4 = fun_3 ++ "1"
+
+fun_5 :: String
+fun_5 = fun_1 ++ fun_4
