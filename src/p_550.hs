73c0d9bed58779d4de3380657b45a23d4c89be86
vil02 <vil02@users.noreply.github.com>
2024-07-30 01:49:01 +0000

    feat: add `p_550`

---
+++ src/p_550.hs
@@ -0,0 +1,57 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_17
+
+fun_0 :: String
+fun_0 = "Y" ++ "v"
+
+fun_1 :: String
+fun_1 = "M" ++ "A"
+
+fun_2 :: String
+fun_2 = "F" ++ fun_1
+
+fun_3 :: String
+fun_3 = "K" ++ "d"
+
+fun_4 :: String
+fun_4 = "p" ++ "x"
+
+fun_5 :: String
+fun_5 = "s" ++ fun_4 ++ "X"
+
+fun_6 :: String
+fun_6 = fun_3 ++ fun_5
+
+fun_7 :: String
+fun_7 = "x" ++ "3"
+
+fun_8 :: String
+fun_8 = "o" ++ "9"
+
+fun_9 :: String
+fun_9 = fun_7 ++ fun_8
+
+fun_10 :: String
+fun_10 = fun_6 ++ "j" ++ fun_9
+
+fun_11 :: String
+fun_11 = fun_2 ++ fun_10 ++ "1" ++ "9"
+
+fun_12 :: String
+fun_12 = "G" ++ "r"
+
+fun_13 :: String
+fun_13 = "b" ++ fun_12 ++ "c"
+
+fun_14 :: String
+fun_14 = fun_11 ++ fun_13
+
+fun_15 :: String
+fun_15 = "q" ++ "f"
+
+fun_16 :: String
+fun_16 = "x" ++ "S"
+
+fun_17 :: String
+fun_17 = fun_0 ++ fun_14 ++ "F" ++ fun_15 ++ fun_16
