7530e5e8973e05321fc752e750a491f8e814738f
vil02 <vil02@users.noreply.github.com>
2024-08-03 22:46:59 +0000

    feat: add `p_564`

---
+++ src/p_564.hs
@@ -0,0 +1,66 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_20
+
+fun_0 :: String
+fun_0 = "2" ++ "G"
+
+fun_1 :: String
+fun_1 = "i" ++ "l"
+
+fun_2 :: String
+fun_2 = fun_1 ++ "o"
+
+fun_3 :: String
+fun_3 = "U" ++ "7" ++ "0"
+
+fun_4 :: String
+fun_4 = "l" ++ "D"
+
+fun_5 :: String
+fun_5 = "u" ++ fun_3 ++ fun_4
+
+fun_6 :: String
+fun_6 = fun_0 ++ fun_2 ++ fun_5
+
+fun_7 :: String
+fun_7 = "i" ++ "W"
+
+fun_8 :: String
+fun_8 = "l" ++ "m"
+
+fun_9 :: String
+fun_9 = fun_7 ++ fun_8
+
+fun_10 :: String
+fun_10 = "K" ++ "o" ++ "Q" ++ fun_9
+
+fun_11 :: String
+fun_11 = "N" ++ "m"
+
+fun_12 :: String
+fun_12 = fun_11 ++ "m"
+
+fun_13 :: String
+fun_13 = fun_12 ++ "Z"
+
+fun_14 :: String
+fun_14 = "i" ++ "U"
+
+fun_15 :: String
+fun_15 = "N" ++ "G"
+
+fun_16 :: String
+fun_16 = fun_14 ++ fun_15
+
+fun_17 :: String
+fun_17 = "x" ++ "y"
+
+fun_18 :: String
+fun_18 = "Q" ++ "m"
+
+fun_19 :: String
+fun_19 = fun_17 ++ fun_18
+
+fun_20 :: String
+fun_20 = fun_6 ++ fun_10 ++ fun_13 ++ fun_16 ++ fun_19
