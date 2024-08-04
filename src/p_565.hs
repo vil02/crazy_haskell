3f9789ab562c5ae2e807aaf7233bc93f8c5cc462
vil02 <vil02@users.noreply.github.com>
2024-08-04 01:58:39 +0000

    feat: add `p_565`

---
+++ src/p_565.hs
@@ -0,0 +1,54 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_16
+
+fun_0 :: String
+fun_0 = "A" ++ "S"
+
+fun_1 :: String
+fun_1 = "K" ++ "T"
+
+fun_2 :: String
+fun_2 = fun_0 ++ fun_1
+
+fun_3 :: String
+fun_3 = "Y" ++ "q"
+
+fun_4 :: String
+fun_4 = "H" ++ "7"
+
+fun_5 :: String
+fun_5 = fun_4 ++ "0"
+
+fun_6 :: String
+fun_6 = "z" ++ "u"
+
+fun_7 :: String
+fun_7 = fun_5 ++ fun_6
+
+fun_8 :: String
+fun_8 = "b" ++ "1" ++ "8"
+
+fun_9 :: String
+fun_9 = "B" ++ "z" ++ "h"
+
+fun_10 :: String
+fun_10 = "J" ++ fun_8 ++ fun_9
+
+fun_11 :: String
+fun_11 = "x" ++ "7"
+
+fun_12 :: String
+fun_12 = "U" ++ "G"
+
+fun_13 :: String
+fun_13 = fun_12 ++ "A"
+
+fun_14 :: String
+fun_14 = "l" ++ fun_13 ++ "2"
+
+fun_15 :: String
+fun_15 = fun_14 ++ "R" ++ "D"
+
+fun_16 :: String
+fun_16 = fun_2 ++ fun_3 ++ "8" ++ fun_7 ++ fun_10 ++ fun_11 ++ fun_15
