83719c7eb4af4f73962659af93553e3f4ab23199
vil02 <vil02@users.noreply.github.com>
2024-08-28 22:46:53 +0000

    feat: add `p_639`

---
+++ src/p_639.hs
@@ -0,0 +1,42 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_12
+
+fun_0 :: String
+fun_0 = "r" ++ "3" ++ "j"
+
+fun_1 :: String
+fun_1 = "y" ++ fun_0
+
+fun_2 :: String
+fun_2 = "m" ++ fun_1
+
+fun_3 :: String
+fun_3 = "a" ++ "0"
+
+fun_4 :: String
+fun_4 = fun_2 ++ "4" ++ fun_3
+
+fun_5 :: String
+fun_5 = "q" ++ "W"
+
+fun_6 :: String
+fun_6 = fun_5 ++ "w"
+
+fun_7 :: String
+fun_7 = "l" ++ "8"
+
+fun_8 :: String
+fun_8 = "n" ++ fun_7 ++ "U"
+
+fun_9 :: String
+fun_9 = "V" ++ fun_8
+
+fun_10 :: String
+fun_10 = "j" ++ fun_9
+
+fun_11 :: String
+fun_11 = fun_6 ++ "f" ++ fun_10 ++ "X"
+
+fun_12 :: String
+fun_12 = fun_4 ++ fun_11
