29748c8e2f8486f00369162a0fbd09b111de079c
vil02 <vil02@users.noreply.github.com>
2024-08-31 22:46:33 +0000

    feat: add `p_648`

---
+++ src/p_648.hs
@@ -0,0 +1,48 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_14
+
+fun_0 :: String
+fun_0 = "y" ++ "1"
+
+fun_1 :: String
+fun_1 = "u" ++ "j"
+
+fun_2 :: String
+fun_2 = "0" ++ "m"
+
+fun_3 :: String
+fun_3 = fun_2 ++ "1"
+
+fun_4 :: String
+fun_4 = "F" ++ "b"
+
+fun_5 :: String
+fun_5 = fun_4 ++ "j"
+
+fun_6 :: String
+fun_6 = fun_1 ++ fun_3 ++ "m" ++ "r" ++ "m" ++ fun_5
+
+fun_7 :: String
+fun_7 = fun_0 ++ "8" ++ fun_6
+
+fun_8 :: String
+fun_8 = "4" ++ "C"
+
+fun_9 :: String
+fun_9 = "k" ++ "K"
+
+fun_10 :: String
+fun_10 = fun_8 ++ fun_9
+
+fun_11 :: String
+fun_11 = fun_10 ++ "h"
+
+fun_12 :: String
+fun_12 = "T" ++ fun_11 ++ "k"
+
+fun_13 :: String
+fun_13 = fun_12 ++ "R" ++ "A" ++ "1"
+
+fun_14 :: String
+fun_14 = fun_7 ++ "1" ++ fun_13
