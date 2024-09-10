c30b376b1a0218c6a9ad76e6d3513d9d876116ac
vil02 <vil02@users.noreply.github.com>
2024-09-10 11:46:46 +0000

    feat: add `p_678`

---
+++ src/p_678.hs
@@ -0,0 +1,42 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_12
+
+fun_0 :: String
+fun_0 = "Y" ++ "Z"
+
+fun_1 :: String
+fun_1 = "J" ++ fun_0
+
+fun_2 :: String
+fun_2 = "f" ++ "l" ++ fun_1 ++ "D"
+
+fun_3 :: String
+fun_3 = "E" ++ "m"
+
+fun_4 :: String
+fun_4 = "p" ++ "e"
+
+fun_5 :: String
+fun_5 = "g" ++ fun_3 ++ fun_4 ++ "i" ++ "6"
+
+fun_6 :: String
+fun_6 = "x" ++ "H"
+
+fun_7 :: String
+fun_7 = "b" ++ fun_6
+
+fun_8 :: String
+fun_8 = "W" ++ "j"
+
+fun_9 :: String
+fun_9 = "i" ++ "t"
+
+fun_10 :: String
+fun_10 = fun_7 ++ "U" ++ "L" ++ fun_8 ++ fun_9
+
+fun_11 :: String
+fun_11 = fun_5 ++ fun_10 ++ "P"
+
+fun_12 :: String
+fun_12 = fun_2 ++ fun_11 ++ "G" ++ "K" ++ "B"
