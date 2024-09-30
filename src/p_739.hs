4b069bb2133f6a615d9a3e6f6efddc07b74c4533
vil02 <vil02@users.noreply.github.com>
2024-09-30 22:46:34 +0000

    feat: add `p_739`

---
+++ src/p_739.hs
@@ -0,0 +1,57 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_17
+
+fun_0 :: String
+fun_0 = "2" ++ "8"
+
+fun_1 :: String
+fun_1 = fun_0 ++ "M"
+
+fun_2 :: String
+fun_2 = "p" ++ "K"
+
+fun_3 :: String
+fun_3 = fun_2 ++ "S" ++ "Z"
+
+fun_4 :: String
+fun_4 = "R" ++ fun_1 ++ "O" ++ fun_3
+
+fun_5 :: String
+fun_5 = "j" ++ "w"
+
+fun_6 :: String
+fun_6 = "9" ++ fun_5
+
+fun_7 :: String
+fun_7 = "w" ++ "I"
+
+fun_8 :: String
+fun_8 = "2" ++ "B" ++ "W"
+
+fun_9 :: String
+fun_9 = "J" ++ fun_8
+
+fun_10 :: String
+fun_10 = fun_6 ++ "a" ++ fun_7 ++ fun_9
+
+fun_11 :: String
+fun_11 = "v" ++ "P"
+
+fun_12 :: String
+fun_12 = "B" ++ "D"
+
+fun_13 :: String
+fun_13 = "V" ++ fun_12
+
+fun_14 :: String
+fun_14 = "T" ++ "6"
+
+fun_15 :: String
+fun_15 = fun_14 ++ "8"
+
+fun_16 :: String
+fun_16 = fun_11 ++ fun_13 ++ fun_15
+
+fun_17 :: String
+fun_17 = fun_4 ++ fun_10 ++ fun_16
