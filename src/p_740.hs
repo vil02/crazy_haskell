f566cd0af8d4082d265c265ed092a53d1996530e
vil02 <vil02@users.noreply.github.com>
2024-10-01 02:14:49 +0000

    feat: add `p_740`

---
+++ src/p_740.hs
@@ -0,0 +1,30 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_8
+
+fun_0 :: String
+fun_0 = "T" ++ "e" ++ "b"
+
+fun_1 :: String
+fun_1 = "p" ++ "C"
+
+fun_2 :: String
+fun_2 = fun_0 ++ fun_1
+
+fun_3 :: String
+fun_3 = "c" ++ "C"
+
+fun_4 :: String
+fun_4 = fun_2 ++ fun_3 ++ "g"
+
+fun_5 :: String
+fun_5 = "L" ++ "D"
+
+fun_6 :: String
+fun_6 = "r" ++ "J"
+
+fun_7 :: String
+fun_7 = fun_5 ++ fun_6 ++ "c"
+
+fun_8 :: String
+fun_8 = "p" ++ fun_4 ++ fun_7 ++ "4"
