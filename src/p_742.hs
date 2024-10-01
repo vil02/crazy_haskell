14cf314241d45f61071dfa6bbe9a4576d7667689
vil02 <vil02@users.noreply.github.com>
2024-10-01 22:46:37 +0000

    feat: add `p_742`

---
+++ src/p_742.hs
@@ -0,0 +1,57 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_17
+
+fun_0 :: String
+fun_0 = "B" ++ "R"
+
+fun_1 :: String
+fun_1 = "P" ++ "y"
+
+fun_2 :: String
+fun_2 = "k" ++ fun_1
+
+fun_3 :: String
+fun_3 = "l" ++ "I"
+
+fun_4 :: String
+fun_4 = "t" ++ "P"
+
+fun_5 :: String
+fun_5 = fun_4 ++ "3" ++ "H"
+
+fun_6 :: String
+fun_6 = "q" ++ "t"
+
+fun_7 :: String
+fun_7 = "d" ++ fun_5 ++ fun_6
+
+fun_8 :: String
+fun_8 = "e" ++ "9"
+
+fun_9 :: String
+fun_9 = fun_8 ++ "0"
+
+fun_10 :: String
+fun_10 = "l" ++ "5"
+
+fun_11 :: String
+fun_11 = fun_7 ++ fun_9 ++ fun_10
+
+fun_12 :: String
+fun_12 = fun_2 ++ "r" ++ "z" ++ fun_3 ++ fun_11
+
+fun_13 :: String
+fun_13 = "J" ++ "u"
+
+fun_14 :: String
+fun_14 = "g" ++ "q"
+
+fun_15 :: String
+fun_15 = fun_14 ++ "7"
+
+fun_16 :: String
+fun_16 = fun_13 ++ fun_15
+
+fun_17 :: String
+fun_17 = fun_0 ++ fun_12 ++ "J" ++ "i" ++ fun_16
