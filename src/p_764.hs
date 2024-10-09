6fecf8c1a83cea1740e0cee487c95a5a32853028
vil02 <vil02@users.noreply.github.com>
2024-10-09 02:05:28 +0000

    feat: add `p_764`

---
+++ src/p_764.hs
@@ -0,0 +1,48 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_14
+
+fun_0 :: String
+fun_0 = "b" ++ "H"
+
+fun_1 :: String
+fun_1 = "S" ++ "t"
+
+fun_2 :: String
+fun_2 = "1" ++ fun_1
+
+fun_3 :: String
+fun_3 = fun_0 ++ fun_2
+
+fun_4 :: String
+fun_4 = "O" ++ "z" ++ "n"
+
+fun_5 :: String
+fun_5 = "Z" ++ "n"
+
+fun_6 :: String
+fun_6 = "q" ++ "o"
+
+fun_7 :: String
+fun_7 = fun_5 ++ fun_6 ++ "0"
+
+fun_8 :: String
+fun_8 = fun_4 ++ fun_7
+
+fun_9 :: String
+fun_9 = "9" ++ "f"
+
+fun_10 :: String
+fun_10 = "s" ++ "l"
+
+fun_11 :: String
+fun_11 = fun_10 ++ "S" ++ "W"
+
+fun_12 :: String
+fun_12 = "F" ++ fun_9 ++ fun_11
+
+fun_13 :: String
+fun_13 = fun_12 ++ "o" ++ "k"
+
+fun_14 :: String
+fun_14 = fun_3 ++ fun_8 ++ "p" ++ "U" ++ fun_13
