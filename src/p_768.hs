fcd91a965f61015722052021fa9161de9c6006aa
vil02 <vil02@users.noreply.github.com>
2024-10-10 11:46:56 +0000

    feat: add `p_768`

---
+++ src/p_768.hs
@@ -0,0 +1,48 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_14
+
+fun_0 :: String
+fun_0 = "I" ++ "x"
+
+fun_1 :: String
+fun_1 = "Y" ++ "k" ++ fun_0
+
+fun_2 :: String
+fun_2 = "P" ++ "H"
+
+fun_3 :: String
+fun_3 = "R" ++ "K" ++ "F" ++ "i"
+
+fun_4 :: String
+fun_4 = fun_2 ++ fun_3
+
+fun_5 :: String
+fun_5 = "A" ++ fun_4
+
+fun_6 :: String
+fun_6 = "H" ++ "k"
+
+fun_7 :: String
+fun_7 = "5" ++ fun_6
+
+fun_8 :: String
+fun_8 = "n" ++ "g" ++ "T" ++ "M"
+
+fun_9 :: String
+fun_9 = "w" ++ "a"
+
+fun_10 :: String
+fun_10 = fun_8 ++ fun_9 ++ "7"
+
+fun_11 :: String
+fun_11 = "c" ++ fun_7 ++ fun_10
+
+fun_12 :: String
+fun_12 = fun_5 ++ fun_11 ++ "s"
+
+fun_13 :: String
+fun_13 = "H" ++ "G"
+
+fun_14 :: String
+fun_14 = "4" ++ "V" ++ "1" ++ fun_1 ++ fun_12 ++ fun_13
