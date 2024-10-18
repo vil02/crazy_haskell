8bf0f973f1975150b375253887ea3b40d39b0968
vil02 <vil02@users.noreply.github.com>
2024-10-18 02:07:19 +0000

    feat: add `p_19`

---
+++ src/p_19.hs
@@ -0,0 +1,57 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_17
+
+fun_0 :: String
+fun_0 = "P" ++ "j"
+
+fun_1 :: String
+fun_1 = "a" ++ "R" ++ "r"
+
+fun_2 :: String
+fun_2 = "g" ++ "n"
+
+fun_3 :: String
+fun_3 = "b" ++ fun_2
+
+fun_4 :: String
+fun_4 = "w" ++ "v" ++ fun_3
+
+fun_5 :: String
+fun_5 = "0" ++ fun_4
+
+fun_6 :: String
+fun_6 = "8" ++ "g"
+
+fun_7 :: String
+fun_7 = fun_6 ++ "q"
+
+fun_8 :: String
+fun_8 = "k" ++ "q" ++ "E" ++ "s"
+
+fun_9 :: String
+fun_9 = "j" ++ fun_7 ++ fun_8
+
+fun_10 :: String
+fun_10 = "E" ++ fun_1 ++ fun_5 ++ fun_9
+
+fun_11 :: String
+fun_11 = "b" ++ "C"
+
+fun_12 :: String
+fun_12 = fun_11 ++ "J"
+
+fun_13 :: String
+fun_13 = "B" ++ "2"
+
+fun_14 :: String
+fun_14 = "U" ++ fun_13
+
+fun_15 :: String
+fun_15 = fun_12 ++ fun_14
+
+fun_16 :: String
+fun_16 = fun_10 ++ fun_15
+
+fun_17 :: String
+fun_17 = fun_0 ++ fun_16
