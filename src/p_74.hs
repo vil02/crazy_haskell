36d6bf5b4059cba065c1c9e1575831c8bacab79a
vil02 <vil02@users.noreply.github.com>
2024-11-05 11:46:41 +0000

    feat: add `p_74`

---
+++ src/p_74.hs
@@ -0,0 +1,15 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_3
+
+fun_0 :: String
+fun_0 = "U" ++ "3" ++ "c"
+
+fun_1 :: String
+fun_1 = "P" ++ "I" ++ "5"
+
+fun_2 :: String
+fun_2 = "j" ++ fun_1 ++ "A"
+
+fun_3 :: String
+fun_3 = fun_0 ++ fun_2
