19f986e6b97a4f1f7d6b85cd3cd02d81b904e869
vil02 <vil02@users.noreply.github.com>
2024-11-25 11:46:50 +0000

    feat: add `p_134`

---
+++ src/p_134.hs
@@ -0,0 +1,57 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_17
+
+fun_0 :: String
+fun_0 = "2" ++ "b"
+
+fun_1 :: String
+fun_1 = "e" ++ "9" ++ "y" ++ fun_0
+
+fun_2 :: String
+fun_2 = "g" ++ "4"
+
+fun_3 :: String
+fun_3 = fun_2 ++ "y"
+
+fun_4 :: String
+fun_4 = fun_1 ++ fun_3
+
+fun_5 :: String
+fun_5 = "1" ++ "d"
+
+fun_6 :: String
+fun_6 = "9" ++ "z"
+
+fun_7 :: String
+fun_7 = fun_6 ++ "5" ++ "s" ++ "a"
+
+fun_8 :: String
+fun_8 = fun_5 ++ fun_7
+
+fun_9 :: String
+fun_9 = "J" ++ "V" ++ "c"
+
+fun_10 :: String
+fun_10 = "z" ++ fun_9
+
+fun_11 :: String
+fun_11 = fun_10 ++ "U"
+
+fun_12 :: String
+fun_12 = "Z" ++ "P"
+
+fun_13 :: String
+fun_13 = fun_11 ++ fun_12
+
+fun_14 :: String
+fun_14 = "l" ++ "k"
+
+fun_15 :: String
+fun_15 = "P" ++ "6"
+
+fun_16 :: String
+fun_16 = fun_8 ++ fun_13 ++ fun_14 ++ fun_15
+
+fun_17 :: String
+fun_17 = fun_4 ++ fun_16 ++ "k"
