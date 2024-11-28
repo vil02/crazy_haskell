4ef400d48b6a356e3ff5a0ce6a16e9d2fa038bd3
vil02 <vil02@users.noreply.github.com>
2024-11-28 02:14:32 +0000

    feat: add `p_142`

---
+++ src/p_142.hs
@@ -0,0 +1,54 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_16
+
+fun_0 :: String
+fun_0 = "D" ++ "N" ++ "B"
+
+fun_1 :: String
+fun_1 = "G" ++ "a"
+
+fun_2 :: String
+fun_2 = "f" ++ "w"
+
+fun_3 :: String
+fun_3 = fun_0 ++ fun_1 ++ fun_2
+
+fun_4 :: String
+fun_4 = "K" ++ "Z"
+
+fun_5 :: String
+fun_5 = "r" ++ fun_3 ++ fun_4
+
+fun_6 :: String
+fun_6 = fun_5 ++ "Z"
+
+fun_7 :: String
+fun_7 = "Y" ++ "C"
+
+fun_8 :: String
+fun_8 = fun_7 ++ "J" ++ "6"
+
+fun_9 :: String
+fun_9 = "U" ++ "P"
+
+fun_10 :: String
+fun_10 = "C" ++ fun_9
+
+fun_11 :: String
+fun_11 = "3" ++ fun_10
+
+fun_12 :: String
+fun_12 = "i" ++ "L" ++ fun_11 ++ "o"
+
+fun_13 :: String
+fun_13 = "G" ++ "N" ++ "T"
+
+fun_14 :: String
+fun_14 = "v" ++ fun_13
+
+fun_15 :: String
+fun_15 = fun_6 ++ fun_8 ++ fun_12 ++ fun_14 ++ "A" ++ "F" ++ "u"
+
+fun_16 :: String
+fun_16 = "C" ++ fun_15
