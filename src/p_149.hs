f31a91c38ac1d85ab70a47c6ea1caf2ab79eda8d
vil02 <vil02@users.noreply.github.com>
2024-11-30 11:46:37 +0000

    feat: add `p_149`

---
+++ src/p_149.hs
@@ -0,0 +1,57 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_17
+
+fun_0 :: String
+fun_0 = "o" ++ "D"
+
+fun_1 :: String
+fun_1 = "4" ++ fun_0
+
+fun_2 :: String
+fun_2 = "8" ++ "f"
+
+fun_3 :: String
+fun_3 = fun_2 ++ "1"
+
+fun_4 :: String
+fun_4 = fun_1 ++ fun_3 ++ "B" ++ "z"
+
+fun_5 :: String
+fun_5 = "o" ++ "V"
+
+fun_6 :: String
+fun_6 = "r" ++ "S"
+
+fun_7 :: String
+fun_7 = "w" ++ fun_6
+
+fun_8 :: String
+fun_8 = fun_7 ++ "I"
+
+fun_9 :: String
+fun_9 = fun_8 ++ "z"
+
+fun_10 :: String
+fun_10 = "J" ++ "3"
+
+fun_11 :: String
+fun_11 = "I" ++ "f"
+
+fun_12 :: String
+fun_12 = fun_10 ++ "v" ++ fun_11
+
+fun_13 :: String
+fun_13 = fun_5 ++ fun_9 ++ fun_12
+
+fun_14 :: String
+fun_14 = "R" ++ fun_13
+
+fun_15 :: String
+fun_15 = fun_4 ++ fun_14 ++ "h"
+
+fun_16 :: String
+fun_16 = "A" ++ "y"
+
+fun_17 :: String
+fun_17 = fun_15 ++ fun_16 ++ "K"
