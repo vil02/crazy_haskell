1fcf200fef8a44e26f029a28a1c493787cc611e2
vil02 <vil02@users.noreply.github.com>
2024-12-07 22:46:48 +0000

    feat: add `p_172`

---
+++ src/p_172.hs
@@ -0,0 +1,60 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_18
+
+fun_0 :: String
+fun_0 = "5" ++ "2"
+
+fun_1 :: String
+fun_1 = "u" ++ "n"
+
+fun_2 :: String
+fun_2 = "C" ++ fun_1
+
+fun_3 :: String
+fun_3 = fun_2 ++ "C"
+
+fun_4 :: String
+fun_4 = "a" ++ "L"
+
+fun_5 :: String
+fun_5 = fun_3 ++ fun_4
+
+fun_6 :: String
+fun_6 = "V" ++ fun_0 ++ fun_5
+
+fun_7 :: String
+fun_7 = "B" ++ "D"
+
+fun_8 :: String
+fun_8 = "K" ++ "V" ++ "z"
+
+fun_9 :: String
+fun_9 = "r" ++ "0"
+
+fun_10 :: String
+fun_10 = "H" ++ fun_9
+
+fun_11 :: String
+fun_11 = "m" ++ "O"
+
+fun_12 :: String
+fun_12 = "e" ++ "w"
+
+fun_13 :: String
+fun_13 = "K" ++ "t"
+
+fun_14 :: String
+fun_14 = fun_12 ++ fun_13
+
+fun_15 :: String
+fun_15 = fun_8 ++ fun_10 ++ fun_11 ++ fun_14
+
+fun_16 :: String
+fun_16 = "E" ++ "b"
+
+fun_17 :: String
+fun_17 = fun_7 ++ fun_15 ++ fun_16
+
+fun_18 :: String
+fun_18 = fun_6 ++ fun_17
