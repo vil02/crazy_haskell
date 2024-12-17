e87243d8271dde0c92a87ec7682118aab06f4584
vil02 <vil02@users.noreply.github.com>
2024-12-17 11:46:47 +0000

    feat: add `p_201`

---
+++ src/p_201.hs
@@ -0,0 +1,60 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_18
+
+fun_0 :: String
+fun_0 = "u" ++ "0"
+
+fun_1 :: String
+fun_1 = fun_0 ++ "B"
+
+fun_2 :: String
+fun_2 = "l" ++ fun_1
+
+fun_3 :: String
+fun_3 = "p" ++ "e" ++ fun_2 ++ "C"
+
+fun_4 :: String
+fun_4 = fun_3 ++ "l"
+
+fun_5 :: String
+fun_5 = "5" ++ "L"
+
+fun_6 :: String
+fun_6 = "0" ++ "k"
+
+fun_7 :: String
+fun_7 = fun_6 ++ "L"
+
+fun_8 :: String
+fun_8 = fun_5 ++ fun_7
+
+fun_9 :: String
+fun_9 = fun_4 ++ fun_8
+
+fun_10 :: String
+fun_10 = "w" ++ "v"
+
+fun_11 :: String
+fun_11 = fun_10 ++ "0"
+
+fun_12 :: String
+fun_12 = "S" ++ fun_9 ++ fun_11 ++ "k"
+
+fun_13 :: String
+fun_13 = "D" ++ "E"
+
+fun_14 :: String
+fun_14 = "C" ++ "t"
+
+fun_15 :: String
+fun_15 = "w" ++ "1"
+
+fun_16 :: String
+fun_16 = fun_14 ++ fun_15
+
+fun_17 :: String
+fun_17 = fun_13 ++ fun_16 ++ "U"
+
+fun_18 :: String
+fun_18 = fun_12 ++ fun_17 ++ "0"
