d01123da86e719d4da85680bd2ea481cc12299d3
vil02 <vil02@users.noreply.github.com>
2024-12-23 11:46:40 +0000

    feat: add `p_219`

---
+++ src/p_219.hs
@@ -0,0 +1,27 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_7
+
+fun_0 :: String
+fun_0 = "y" ++ "W"
+
+fun_1 :: String
+fun_1 = "1" ++ "f"
+
+fun_2 :: String
+fun_2 = fun_1 ++ "d"
+
+fun_3 :: String
+fun_3 = "X" ++ "P" ++ "W"
+
+fun_4 :: String
+fun_4 = fun_0 ++ "A" ++ fun_2 ++ fun_3
+
+fun_5 :: String
+fun_5 = "W" ++ "U"
+
+fun_6 :: String
+fun_6 = "P" ++ fun_5
+
+fun_7 :: String
+fun_7 = fun_4 ++ fun_6
