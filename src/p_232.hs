dbac1f0906968653ed7e6192c829e33b150b1794
vil02 <vil02@users.noreply.github.com>
2024-12-27 22:46:34 +0000

    feat: add `p_232`

---
+++ src/p_232.hs
@@ -0,0 +1,57 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_17
+
+fun_0 :: String
+fun_0 = "8" ++ "H"
+
+fun_1 :: String
+fun_1 = "F" ++ "L"
+
+fun_2 :: String
+fun_2 = "G" ++ fun_1
+
+fun_3 :: String
+fun_3 = "e" ++ "g"
+
+fun_4 :: String
+fun_4 = fun_0 ++ fun_2 ++ fun_3 ++ "0" ++ "Y"
+
+fun_5 :: String
+fun_5 = fun_4 ++ "R"
+
+fun_6 :: String
+fun_6 = fun_5 ++ "o"
+
+fun_7 :: String
+fun_7 = "g" ++ fun_6
+
+fun_8 :: String
+fun_8 = "j" ++ "G"
+
+fun_9 :: String
+fun_9 = "F" ++ "I" ++ "V"
+
+fun_10 :: String
+fun_10 = fun_9 ++ "p"
+
+fun_11 :: String
+fun_11 = "Y" ++ "P"
+
+fun_12 :: String
+fun_12 = fun_11 ++ "Y"
+
+fun_13 :: String
+fun_13 = "6" ++ fun_12
+
+fun_14 :: String
+fun_14 = "o" ++ fun_13
+
+fun_15 :: String
+fun_15 = fun_10 ++ fun_14
+
+fun_16 :: String
+fun_16 = fun_8 ++ fun_15
+
+fun_17 :: String
+fun_17 = fun_7 ++ fun_16 ++ "c"
