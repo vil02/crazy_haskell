f1ffb9793d925f3cb7fb527feb829660149b9633
vil02 <vil02@users.noreply.github.com>
2025-01-01 02:13:09 +0000

    feat: add `p_245`

---
+++ src/p_245.hs
@@ -0,0 +1,54 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_16
+
+fun_0 :: String
+fun_0 = "n" ++ "K" ++ "g"
+
+fun_1 :: String
+fun_1 = "n" ++ "U"
+
+fun_2 :: String
+fun_2 = fun_0 ++ "6" ++ fun_1
+
+fun_3 :: String
+fun_3 = "M" ++ "l"
+
+fun_4 :: String
+fun_4 = "6" ++ "Y" ++ "X"
+
+fun_5 :: String
+fun_5 = "F" ++ "i"
+
+fun_6 :: String
+fun_6 = fun_5 ++ "9"
+
+fun_7 :: String
+fun_7 = "T" ++ "g" ++ fun_6
+
+fun_8 :: String
+fun_8 = "A" ++ fun_7 ++ "X"
+
+fun_9 :: String
+fun_9 = "B" ++ "c"
+
+fun_10 :: String
+fun_10 = fun_8 ++ fun_9
+
+fun_11 :: String
+fun_11 = "1" ++ "m"
+
+fun_12 :: String
+fun_12 = "W" ++ "Y"
+
+fun_13 :: String
+fun_13 = "o" ++ "x"
+
+fun_14 :: String
+fun_14 = "F" ++ fun_12 ++ "6" ++ fun_13
+
+fun_15 :: String
+fun_15 = fun_4 ++ fun_10 ++ fun_11 ++ fun_14
+
+fun_16 :: String
+fun_16 = fun_2 ++ fun_3 ++ fun_15 ++ "I"
