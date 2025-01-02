69c64a3df72c930c0031d00b2dc88922c2076679
vil02 <vil02@users.noreply.github.com>
2025-01-02 22:46:47 +0000

    feat: add `p_250`

---
+++ src/p_250.hs
@@ -0,0 +1,57 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_17
+
+fun_0 :: String
+fun_0 = "8" ++ "Q" ++ "W"
+
+fun_1 :: String
+fun_1 = "F" ++ fun_0
+
+fun_2 :: String
+fun_2 = "V" ++ fun_1
+
+fun_3 :: String
+fun_3 = "H" ++ "S"
+
+fun_4 :: String
+fun_4 = "q" ++ fun_3 ++ "e"
+
+fun_5 :: String
+fun_5 = "I" ++ "3"
+
+fun_6 :: String
+fun_6 = fun_4 ++ fun_5
+
+fun_7 :: String
+fun_7 = "2" ++ "S"
+
+fun_8 :: String
+fun_8 = fun_7 ++ "H" ++ "a"
+
+fun_9 :: String
+fun_9 = "y" ++ fun_8
+
+fun_10 :: String
+fun_10 = "X" ++ "T"
+
+fun_11 :: String
+fun_11 = fun_2 ++ fun_6 ++ fun_9 ++ fun_10
+
+fun_12 :: String
+fun_12 = "1" ++ "A"
+
+fun_13 :: String
+fun_13 = "5" ++ "O"
+
+fun_14 :: String
+fun_14 = "a" ++ "d"
+
+fun_15 :: String
+fun_15 = fun_13 ++ fun_14
+
+fun_16 :: String
+fun_16 = fun_12 ++ fun_15 ++ "U" ++ "A"
+
+fun_17 :: String
+fun_17 = fun_11 ++ fun_16 ++ "d"
