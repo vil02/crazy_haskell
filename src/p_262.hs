c831e8b4d36b5ee9761898cbd20d7c63e407f8bd
vil02 <vil02@users.noreply.github.com>
2025-01-06 22:46:47 +0000

    feat: add `p_262`

---
+++ src/p_262.hs
@@ -0,0 +1,57 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_17
+
+fun_0 :: String
+fun_0 = "A" ++ "d"
+
+fun_1 :: String
+fun_1 = fun_0 ++ "n" ++ "7"
+
+fun_2 :: String
+fun_2 = "I" ++ "2"
+
+fun_3 :: String
+fun_3 = "N" ++ "9"
+
+fun_4 :: String
+fun_4 = "e" ++ "v"
+
+fun_5 :: String
+fun_5 = "a" ++ "n"
+
+fun_6 :: String
+fun_6 = "G" ++ "1"
+
+fun_7 :: String
+fun_7 = fun_4 ++ fun_5 ++ fun_6
+
+fun_8 :: String
+fun_8 = "M" ++ "3"
+
+fun_9 :: String
+fun_9 = fun_8 ++ "T" ++ "S"
+
+fun_10 :: String
+fun_10 = "1" ++ fun_9
+
+fun_11 :: String
+fun_11 = "K" ++ "p" ++ "I"
+
+fun_12 :: String
+fun_12 = "3" ++ fun_11
+
+fun_13 :: String
+fun_13 = fun_12 ++ "w"
+
+fun_14 :: String
+fun_14 = "B" ++ fun_10 ++ fun_13 ++ "C"
+
+fun_15 :: String
+fun_15 = fun_3 ++ fun_7 ++ "7" ++ fun_14
+
+fun_16 :: String
+fun_16 = fun_2 ++ "J" ++ "Z" ++ fun_15
+
+fun_17 :: String
+fun_17 = fun_1 ++ fun_16 ++ "F"
