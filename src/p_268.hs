09d16d1bde4af6852b4e221e8ce335ef7bbff009
vil02 <vil02@users.noreply.github.com>
2025-01-08 22:46:46 +0000

    feat: add `p_268`

---
+++ src/p_268.hs
@@ -0,0 +1,54 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_16
+
+fun_0 :: String
+fun_0 = "I" ++ "I"
+
+fun_1 :: String
+fun_1 = fun_0 ++ "q" ++ "S"
+
+fun_2 :: String
+fun_2 = "z" ++ "e"
+
+fun_3 :: String
+fun_3 = "u" ++ "4"
+
+fun_4 :: String
+fun_4 = "k" ++ "G"
+
+fun_5 :: String
+fun_5 = "f" ++ fun_3 ++ fun_4
+
+fun_6 :: String
+fun_6 = "8" ++ fun_1 ++ fun_2 ++ fun_5
+
+fun_7 :: String
+fun_7 = "4" ++ "e" ++ "B"
+
+fun_8 :: String
+fun_8 = "a" ++ fun_7 ++ "a"
+
+fun_9 :: String
+fun_9 = fun_8 ++ "Q"
+
+fun_10 :: String
+fun_10 = fun_9 ++ "C" ++ "z"
+
+fun_11 :: String
+fun_11 = "D" ++ fun_10
+
+fun_12 :: String
+fun_12 = fun_6 ++ "J" ++ fun_11
+
+fun_13 :: String
+fun_13 = "R" ++ "V"
+
+fun_14 :: String
+fun_14 = "E" ++ "5"
+
+fun_15 :: String
+fun_15 = "1" ++ "c"
+
+fun_16 :: String
+fun_16 = fun_12 ++ fun_13 ++ fun_14 ++ fun_15
