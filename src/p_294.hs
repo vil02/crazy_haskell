3e51ed2cab189d14a322cd91100c86f25f05a1f2
vil02 <vil02@users.noreply.github.com>
2025-01-17 11:46:39 +0000

    feat: add `p_294`

---
+++ src/p_294.hs
@@ -0,0 +1,21 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_5
+
+fun_0 :: String
+fun_0 = "d" ++ "F"
+
+fun_1 :: String
+fun_1 = fun_0 ++ "F"
+
+fun_2 :: String
+fun_2 = "k" ++ "4"
+
+fun_3 :: String
+fun_3 = fun_1 ++ "L" ++ "Y" ++ "f" ++ fun_2
+
+fun_4 :: String
+fun_4 = "T" ++ "A"
+
+fun_5 :: String
+fun_5 = fun_3 ++ fun_4
