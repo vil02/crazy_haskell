275e2377dcf7da9deff7c087b2ff64e494c5bf61
vil02 <vil02@users.noreply.github.com>
2025-02-03 11:46:32 +0000

    feat: add `p_345`

---
+++ src/p_345.hs
@@ -0,0 +1,42 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_12
+
+fun_0 :: String
+fun_0 = "C" ++ "j"
+
+fun_1 :: String
+fun_1 = "k" ++ fun_0
+
+fun_2 :: String
+fun_2 = fun_1 ++ "4" ++ "N"
+
+fun_3 :: String
+fun_3 = "g" ++ "T"
+
+fun_4 :: String
+fun_4 = "f" ++ fun_3
+
+fun_5 :: String
+fun_5 = "y" ++ fun_4
+
+fun_6 :: String
+fun_6 = "J" ++ "3" ++ "q"
+
+fun_7 :: String
+fun_7 = "0" ++ fun_5 ++ fun_6
+
+fun_8 :: String
+fun_8 = "j" ++ "j"
+
+fun_9 :: String
+fun_9 = fun_8 ++ "o"
+
+fun_10 :: String
+fun_10 = "v" ++ fun_9
+
+fun_11 :: String
+fun_11 = "w" ++ "c"
+
+fun_12 :: String
+fun_12 = fun_2 ++ fun_7 ++ "3" ++ fun_10 ++ fun_11
