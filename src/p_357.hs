3b45484b5a2b554d62c4294d630c774e1448dd63
vil02 <vil02@users.noreply.github.com>
2025-02-07 11:46:33 +0000

    feat: add `p_357`

---
+++ src/p_357.hs
@@ -0,0 +1,39 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_11
+
+fun_0 :: String
+fun_0 = "j" ++ "s" ++ "J" ++ "f"
+
+fun_1 :: String
+fun_1 = "K" ++ "z"
+
+fun_2 :: String
+fun_2 = "G" ++ fun_1
+
+fun_3 :: String
+fun_3 = "E" ++ "S"
+
+fun_4 :: String
+fun_4 = fun_3 ++ "M"
+
+fun_5 :: String
+fun_5 = fun_0 ++ fun_2 ++ fun_4 ++ "b" ++ "Q"
+
+fun_6 :: String
+fun_6 = "r" ++ "a"
+
+fun_7 :: String
+fun_7 = fun_6 ++ "m"
+
+fun_8 :: String
+fun_8 = "H" ++ "D"
+
+fun_9 :: String
+fun_9 = fun_7 ++ fun_8
+
+fun_10 :: String
+fun_10 = "d" ++ fun_9
+
+fun_11 :: String
+fun_11 = fun_5 ++ "v" ++ fun_10 ++ "c"
