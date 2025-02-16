df8c503374e4a92a5d48f7b3455a240919a17884
vil02 <vil02@users.noreply.github.com>
2025-02-16 11:46:51 +0000

    feat: add `p_384`

---
+++ src/p_384.hs
@@ -0,0 +1,60 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_18
+
+fun_0 :: String
+fun_0 = "Q" ++ "k"
+
+fun_1 :: String
+fun_1 = "h" ++ "T"
+
+fun_2 :: String
+fun_2 = fun_0 ++ fun_1
+
+fun_3 :: String
+fun_3 = "d" ++ "K"
+
+fun_4 :: String
+fun_4 = "r" ++ "u" ++ "S"
+
+fun_5 :: String
+fun_5 = "N" ++ fun_4
+
+fun_6 :: String
+fun_6 = fun_5 ++ "8" ++ "B"
+
+fun_7 :: String
+fun_7 = "M" ++ "D" ++ fun_6 ++ "b" ++ "y"
+
+fun_8 :: String
+fun_8 = "x" ++ "v"
+
+fun_9 :: String
+fun_9 = "R" ++ "T"
+
+fun_10 :: String
+fun_10 = fun_8 ++ fun_9
+
+fun_11 :: String
+fun_11 = "f" ++ "A"
+
+fun_12 :: String
+fun_12 = "Q" ++ fun_11
+
+fun_13 :: String
+fun_13 = fun_12 ++ "g"
+
+fun_14 :: String
+fun_14 = fun_10 ++ fun_13
+
+fun_15 :: String
+fun_15 = fun_14 ++ "P"
+
+fun_16 :: String
+fun_16 = "R" ++ "b" ++ "H" ++ fun_15 ++ "u"
+
+fun_17 :: String
+fun_17 = "K" ++ "M"
+
+fun_18 :: String
+fun_18 = fun_2 ++ fun_3 ++ "R" ++ fun_7 ++ fun_16 ++ fun_17
