a02580a2cbeb3f073d65fa6b96fad7f67113981b
vil02 <vil02@users.noreply.github.com>
2025-02-18 02:04:37 +0000

    feat: add `p_389`

---
+++ src/p_389.hs
@@ -0,0 +1,66 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_20
+
+fun_0 :: String
+fun_0 = "d" ++ "3"
+
+fun_1 :: String
+fun_1 = "W" ++ fun_0
+
+fun_2 :: String
+fun_2 = "X" ++ "a"
+
+fun_3 :: String
+fun_3 = fun_2 ++ "A"
+
+fun_4 :: String
+fun_4 = "r" ++ "w"
+
+fun_5 :: String
+fun_5 = "B" ++ "q"
+
+fun_6 :: String
+fun_6 = fun_4 ++ "s" ++ "3" ++ fun_5
+
+fun_7 :: String
+fun_7 = "v" ++ "r"
+
+fun_8 :: String
+fun_8 = "5" ++ "A" ++ "t"
+
+fun_9 :: String
+fun_9 = "q" ++ fun_8
+
+fun_10 :: String
+fun_10 = fun_7 ++ "m" ++ fun_9
+
+fun_11 :: String
+fun_11 = "8" ++ fun_3 ++ fun_6 ++ fun_10 ++ "d"
+
+fun_12 :: String
+fun_12 = "b" ++ fun_1 ++ fun_11
+
+fun_13 :: String
+fun_13 = "T" ++ "r"
+
+fun_14 :: String
+fun_14 = "u" ++ "X" ++ fun_13
+
+fun_15 :: String
+fun_15 = "h" ++ "p" ++ "R"
+
+fun_16 :: String
+fun_16 = "D" ++ "3" ++ fun_15
+
+fun_17 :: String
+fun_17 = fun_16 ++ "L"
+
+fun_18 :: String
+fun_18 = fun_14 ++ fun_17
+
+fun_19 :: String
+fun_19 = "s" ++ "7"
+
+fun_20 :: String
+fun_20 = fun_12 ++ "r" ++ fun_18 ++ fun_19
