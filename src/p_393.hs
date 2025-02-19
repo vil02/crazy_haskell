c1d0c28ee88bf4e666d30a7f586302e1f19a2e83
vil02 <vil02@users.noreply.github.com>
2025-02-19 11:46:43 +0000

    feat: add `p_393`

---
+++ src/p_393.hs
@@ -0,0 +1,54 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_16
+
+fun_0 :: String
+fun_0 = "O" ++ "a"
+
+fun_1 :: String
+fun_1 = "J" ++ "X"
+
+fun_2 :: String
+fun_2 = "H" ++ "3"
+
+fun_3 :: String
+fun_3 = "W" ++ fun_1 ++ fun_2
+
+fun_4 :: String
+fun_4 = "Q" ++ "q"
+
+fun_5 :: String
+fun_5 = "Y" ++ "x"
+
+fun_6 :: String
+fun_6 = "N" ++ fun_5
+
+fun_7 :: String
+fun_7 = fun_0 ++ fun_3 ++ fun_4 ++ fun_6
+
+fun_8 :: String
+fun_8 = "U" ++ "o"
+
+fun_9 :: String
+fun_9 = fun_7 ++ "p" ++ "g" ++ "k" ++ "E" ++ fun_8 ++ "9"
+
+fun_10 :: String
+fun_10 = "p" ++ "0"
+
+fun_11 :: String
+fun_11 = "y" ++ "1"
+
+fun_12 :: String
+fun_12 = fun_10 ++ "P" ++ fun_11
+
+fun_13 :: String
+fun_13 = "o" ++ "4"
+
+fun_14 :: String
+fun_14 = fun_12 ++ fun_13
+
+fun_15 :: String
+fun_15 = fun_9 ++ fun_14
+
+fun_16 :: String
+fun_16 = "E" ++ fun_15
