80a0aaad29e5dedbacb91ffcebc3a86246313552
vil02 <vil02@users.noreply.github.com>
2025-02-23 02:11:46 +0000

    feat: add `p_404`

---
+++ src/p_404.hs
@@ -0,0 +1,57 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_17
+
+fun_0 :: String
+fun_0 = "O" ++ "c"
+
+fun_1 :: String
+fun_1 = "9" ++ "4"
+
+fun_2 :: String
+fun_2 = fun_1 ++ "w"
+
+fun_3 :: String
+fun_3 = "6" ++ "U"
+
+fun_4 :: String
+fun_4 = fun_2 ++ fun_3
+
+fun_5 :: String
+fun_5 = "o" ++ "6"
+
+fun_6 :: String
+fun_6 = fun_4 ++ fun_5
+
+fun_7 :: String
+fun_7 = "2" ++ "j"
+
+fun_8 :: String
+fun_8 = fun_7 ++ "9"
+
+fun_9 :: String
+fun_9 = fun_6 ++ fun_8
+
+fun_10 :: String
+fun_10 = "q" ++ "H"
+
+fun_11 :: String
+fun_11 = "3" ++ "G"
+
+fun_12 :: String
+fun_12 = "c" ++ fun_11
+
+fun_13 :: String
+fun_13 = fun_10 ++ fun_12 ++ "5"
+
+fun_14 :: String
+fun_14 = "y" ++ "H"
+
+fun_15 :: String
+fun_15 = "a" ++ "o"
+
+fun_16 :: String
+fun_16 = fun_13 ++ fun_14 ++ fun_15
+
+fun_17 :: String
+fun_17 = "f" ++ "l" ++ fun_0 ++ fun_9 ++ fun_16
