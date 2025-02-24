743230553f2633ca0bc1d3e75c2ffef517179a54
vil02 <vil02@users.noreply.github.com>
2025-02-24 11:46:46 +0000

    feat: add `p_408`

---
+++ src/p_408.hs
@@ -0,0 +1,27 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_7
+
+fun_0 :: String
+fun_0 = "j" ++ "j"
+
+fun_1 :: String
+fun_1 = fun_0 ++ "2"
+
+fun_2 :: String
+fun_2 = "I" ++ fun_1
+
+fun_3 :: String
+fun_3 = "u" ++ "d" ++ "y"
+
+fun_4 :: String
+fun_4 = "0" ++ fun_3 ++ "p"
+
+fun_5 :: String
+fun_5 = "v" ++ "y"
+
+fun_6 :: String
+fun_6 = fun_4 ++ "T" ++ fun_5
+
+fun_7 :: String
+fun_7 = "d" ++ fun_2 ++ fun_6
