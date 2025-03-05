6277660003380e286ee687a8bad3c3da6d143a39
vil02 <vil02@users.noreply.github.com>
2025-03-05 02:10:31 +0000

    feat: add `p_435`

---
+++ src/p_435.hs
@@ -0,0 +1,54 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_16
+
+fun_0 :: String
+fun_0 = "Q" ++ "r"
+
+fun_1 :: String
+fun_1 = "b" ++ "8"
+
+fun_2 :: String
+fun_2 = "K" ++ "p" ++ fun_1 ++ "B"
+
+fun_3 :: String
+fun_3 = "i" ++ "m"
+
+fun_4 :: String
+fun_4 = "3" ++ "v" ++ "J"
+
+fun_5 :: String
+fun_5 = "u" ++ fun_4
+
+fun_6 :: String
+fun_6 = fun_3 ++ "c" ++ fun_5
+
+fun_7 :: String
+fun_7 = fun_2 ++ fun_6 ++ "7"
+
+fun_8 :: String
+fun_8 = "v" ++ "z"
+
+fun_9 :: String
+fun_9 = "X" ++ "2"
+
+fun_10 :: String
+fun_10 = fun_8 ++ fun_9
+
+fun_11 :: String
+fun_11 = fun_10 ++ "3" ++ "P"
+
+fun_12 :: String
+fun_12 = "9" ++ "X"
+
+fun_13 :: String
+fun_13 = "0" ++ fun_12
+
+fun_14 :: String
+fun_14 = fun_11 ++ fun_13
+
+fun_15 :: String
+fun_15 = "Q" ++ "z"
+
+fun_16 :: String
+fun_16 = fun_0 ++ fun_7 ++ "t" ++ fun_14 ++ fun_15
