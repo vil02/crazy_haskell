c31292a8e1fd7061fb1bd9d41212a1fbabcf9f22
vil02 <vil02@users.noreply.github.com>
2025-03-08 11:47:03 +0000

    feat: add `p_445`

---
+++ src/p_445.hs
@@ -0,0 +1,57 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_17
+
+fun_0 :: String
+fun_0 = "o" ++ "a"
+
+fun_1 :: String
+fun_1 = fun_0 ++ "k"
+
+fun_2 :: String
+fun_2 = fun_1 ++ "D"
+
+fun_3 :: String
+fun_3 = "K" ++ "k"
+
+fun_4 :: String
+fun_4 = fun_3 ++ "C"
+
+fun_5 :: String
+fun_5 = "C" ++ "I"
+
+fun_6 :: String
+fun_6 = "6" ++ "K"
+
+fun_7 :: String
+fun_7 = "k" ++ "y"
+
+fun_8 :: String
+fun_8 = fun_6 ++ fun_7
+
+fun_9 :: String
+fun_9 = fun_5 ++ "a" ++ fun_8
+
+fun_10 :: String
+fun_10 = fun_9 ++ "R"
+
+fun_11 :: String
+fun_11 = fun_2 ++ fun_4 ++ fun_10
+
+fun_12 :: String
+fun_12 = "5" ++ "1"
+
+fun_13 :: String
+fun_13 = fun_12 ++ "O"
+
+fun_14 :: String
+fun_14 = fun_13 ++ "w" ++ "l"
+
+fun_15 :: String
+fun_15 = fun_11 ++ "f" ++ fun_14
+
+fun_16 :: String
+fun_16 = "1" ++ "I"
+
+fun_17 :: String
+fun_17 = fun_15 ++ fun_16 ++ "f" ++ "N"
