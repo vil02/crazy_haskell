ce326d2b30f436159fee207698a3817575503da2
vil02 <vil02@users.noreply.github.com>
2025-03-10 01:47:43 +0000

    feat: add `p_450`

---
+++ src/p_450.hs
@@ -0,0 +1,57 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_17
+
+fun_0 :: String
+fun_0 = "q" ++ "Y"
+
+fun_1 :: String
+fun_1 = "z" ++ fun_0
+
+fun_2 :: String
+fun_2 = fun_1 ++ "j"
+
+fun_3 :: String
+fun_3 = "W" ++ fun_2
+
+fun_4 :: String
+fun_4 = "D" ++ "X" ++ "X" ++ fun_3 ++ "G"
+
+fun_5 :: String
+fun_5 = fun_4 ++ "0" ++ "M"
+
+fun_6 :: String
+fun_6 = "8" ++ "s"
+
+fun_7 :: String
+fun_7 = "G" ++ "2"
+
+fun_8 :: String
+fun_8 = "V" ++ "K"
+
+fun_9 :: String
+fun_9 = "c" ++ "L"
+
+fun_10 :: String
+fun_10 = "R" ++ "b"
+
+fun_11 :: String
+fun_11 = "U" ++ "L"
+
+fun_12 :: String
+fun_12 = fun_11 ++ "K"
+
+fun_13 :: String
+fun_13 = "E" ++ "G"
+
+fun_14 :: String
+fun_14 = "i" ++ fun_13
+
+fun_15 :: String
+fun_15 = fun_8 ++ "w" ++ fun_9 ++ "u" ++ fun_10 ++ fun_12 ++ fun_14
+
+fun_16 :: String
+fun_16 = "l" ++ fun_5 ++ fun_6 ++ fun_7 ++ fun_15
+
+fun_17 :: String
+fun_17 = "p" ++ fun_16
