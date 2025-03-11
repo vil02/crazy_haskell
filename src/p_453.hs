837a5533d3d6b4322e909551e9e8fa94231bf16b
vil02 <vil02@users.noreply.github.com>
2025-03-11 02:11:29 +0000

    feat: add `p_453`

---
+++ src/p_453.hs
@@ -0,0 +1,33 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_9
+
+fun_0 :: String
+fun_0 = "G" ++ "J"
+
+fun_1 :: String
+fun_1 = "Z" ++ "L"
+
+fun_2 :: String
+fun_2 = fun_1 ++ "C"
+
+fun_3 :: String
+fun_3 = "4" ++ "2" ++ "4"
+
+fun_4 :: String
+fun_4 = fun_0 ++ fun_2 ++ fun_3
+
+fun_5 :: String
+fun_5 = "B" ++ "H"
+
+fun_6 :: String
+fun_6 = "8" ++ "u"
+
+fun_7 :: String
+fun_7 = "b" ++ fun_6
+
+fun_8 :: String
+fun_8 = "x" ++ "N"
+
+fun_9 :: String
+fun_9 = fun_4 ++ fun_5 ++ fun_7 ++ fun_8
