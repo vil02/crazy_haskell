e6c2d6c847b6d8f15775fa7dc62398044111f383
vil02 <vil02@users.noreply.github.com>
2025-03-27 11:46:42 +0000

    feat: add `p_502`

---
+++ src/p_502.hs
@@ -0,0 +1,57 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_17
+
+fun_0 :: String
+fun_0 = "h" ++ "5"
+
+fun_1 :: String
+fun_1 = "f" ++ "s"
+
+fun_2 :: String
+fun_2 = fun_1 ++ "q"
+
+fun_3 :: String
+fun_3 = "T" ++ fun_2
+
+fun_4 :: String
+fun_4 = fun_0 ++ fun_3 ++ "c"
+
+fun_5 :: String
+fun_5 = "5" ++ "s"
+
+fun_6 :: String
+fun_6 = "z" ++ "Z"
+
+fun_7 :: String
+fun_7 = "s" ++ "Y"
+
+fun_8 :: String
+fun_8 = fun_6 ++ fun_7
+
+fun_9 :: String
+fun_9 = "I" ++ fun_8 ++ "v"
+
+fun_10 :: String
+fun_10 = "A" ++ "y"
+
+fun_11 :: String
+fun_11 = "e" ++ fun_5 ++ "Q" ++ fun_9 ++ "f" ++ fun_10
+
+fun_12 :: String
+fun_12 = fun_4 ++ fun_11 ++ "L" ++ "v"
+
+fun_13 :: String
+fun_13 = "P" ++ "t"
+
+fun_14 :: String
+fun_14 = fun_13 ++ "X"
+
+fun_15 :: String
+fun_15 = "4" ++ "b"
+
+fun_16 :: String
+fun_16 = fun_14 ++ fun_15
+
+fun_17 :: String
+fun_17 = fun_12 ++ fun_16
