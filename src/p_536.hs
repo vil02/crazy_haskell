9e76c2443da924169a1e7013caa8abb10a182fc1
vil02 <vil02@users.noreply.github.com>
2025-04-07 22:46:46 +0000

    feat: add `p_536`

---
+++ src/p_536.hs
@@ -0,0 +1,54 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_16
+
+fun_0 :: String
+fun_0 = "O" ++ "d"
+
+fun_1 :: String
+fun_1 = "l" ++ "W"
+
+fun_2 :: String
+fun_2 = "0" ++ "l"
+
+fun_3 :: String
+fun_3 = "s" ++ "w"
+
+fun_4 :: String
+fun_4 = fun_2 ++ fun_3
+
+fun_5 :: String
+fun_5 = "f" ++ "o"
+
+fun_6 :: String
+fun_6 = "f" ++ "P" ++ fun_5
+
+fun_7 :: String
+fun_7 = fun_2 ++ fun_4 ++ fun_6 ++ "3"
+
+fun_8 :: String
+fun_8 = "u" ++ "j"
+
+fun_9 :: String
+fun_9 = "m" ++ "e"
+
+fun_10 :: String
+fun_10 = fun_8 ++ fun_9
+
+fun_11 :: String
+fun_11 = fun_7 ++ "i" ++ fun_10
+
+fun_12 :: String
+fun_12 = fun_11 ++ "N"
+
+fun_13 :: String
+fun_13 = "4" ++ "g"
+
+fun_14 :: String
+fun_14 = "z" ++ "u" ++ fun_13
+
+fun_15 :: String
+fun_15 = fun_12 ++ fun_14 ++ "d"
+
+fun_16 :: String
+fun_16 = fun_0 ++ fun_1 ++ fun_15
