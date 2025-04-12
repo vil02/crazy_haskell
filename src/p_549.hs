9ea34857e6e5f8f5885fbe7c11e79fdf02ac7c6a
vil02 <vil02@users.noreply.github.com>
2025-04-12 02:12:30 +0000

    feat: add `p_549`

---
+++ src/p_549.hs
@@ -0,0 +1,54 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_16
+
+fun_0 :: String
+fun_0 = "e" ++ "o"
+
+fun_1 :: String
+fun_1 = fun_0 ++ "t"
+
+fun_2 :: String
+fun_2 = "l" ++ "7"
+
+fun_3 :: String
+fun_3 = fun_2 ++ "3" ++ "p"
+
+fun_4 :: String
+fun_4 = fun_1 ++ fun_3 ++ "X" ++ "X"
+
+fun_5 :: String
+fun_5 = "s" ++ "z" ++ fun_4
+
+fun_6 :: String
+fun_6 = "K" ++ "S"
+
+fun_7 :: String
+fun_7 = "B" ++ "l"
+
+fun_8 :: String
+fun_8 = fun_7 ++ "u"
+
+fun_9 :: String
+fun_9 = "4" ++ "V" ++ "C"
+
+fun_10 :: String
+fun_10 = fun_9 ++ "E"
+
+fun_11 :: String
+fun_11 = "l" ++ fun_10 ++ "f"
+
+fun_12 :: String
+fun_12 = fun_11 ++ "L" ++ "u"
+
+fun_13 :: String
+fun_13 = "P" ++ "L" ++ fun_8 ++ "v" ++ fun_12
+
+fun_14 :: String
+fun_14 = "P" ++ "R"
+
+fun_15 :: String
+fun_15 = fun_6 ++ "l" ++ fun_13 ++ fun_14
+
+fun_16 :: String
+fun_16 = "C" ++ fun_5 ++ fun_15
