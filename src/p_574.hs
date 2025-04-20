8d2cd3c4bfbff0325700cc63b33ad32fdbafdbac
vil02 <vil02@users.noreply.github.com>
2025-04-20 11:46:36 +0000

    feat: add `p_574`

---
+++ src/p_574.hs
@@ -0,0 +1,57 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_17
+
+fun_0 :: String
+fun_0 = "J" ++ "p"
+
+fun_1 :: String
+fun_1 = "Y" ++ "I" ++ "u"
+
+fun_2 :: String
+fun_2 = "n" ++ "F"
+
+fun_3 :: String
+fun_3 = fun_2 ++ "H" ++ "C" ++ "F"
+
+fun_4 :: String
+fun_4 = fun_1 ++ fun_3
+
+fun_5 :: String
+fun_5 = "R" ++ "3"
+
+fun_6 :: String
+fun_6 = fun_4 ++ fun_5
+
+fun_7 :: String
+fun_7 = "a" ++ "k" ++ fun_0 ++ "F" ++ fun_6
+
+fun_8 :: String
+fun_8 = "i" ++ fun_7
+
+fun_9 :: String
+fun_9 = "v" ++ "U"
+
+fun_10 :: String
+fun_10 = "d" ++ "i"
+
+fun_11 :: String
+fun_11 = "U" ++ "S"
+
+fun_12 :: String
+fun_12 = fun_10 ++ fun_11
+
+fun_13 :: String
+fun_13 = "L" ++ fun_12
+
+fun_14 :: String
+fun_14 = "Z" ++ "k"
+
+fun_15 :: String
+fun_15 = "7" ++ "F"
+
+fun_16 :: String
+fun_16 = fun_9 ++ fun_13 ++ fun_14 ++ fun_15
+
+fun_17 :: String
+fun_17 = fun_8 ++ fun_16
