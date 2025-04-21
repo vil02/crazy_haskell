a0e3b8a86eb8caac5b398182ac1dc058ff6d0858
vil02 <vil02@users.noreply.github.com>
2025-04-21 11:46:45 +0000

    feat: add `p_577`

---
+++ src/p_577.hs
@@ -0,0 +1,21 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_5
+
+fun_0 :: String
+fun_0 = "X" ++ "l" ++ "U"
+
+fun_1 :: String
+fun_1 = "N" ++ "3" ++ fun_0
+
+fun_2 :: String
+fun_2 = "x" ++ "u"
+
+fun_3 :: String
+fun_3 = fun_2 ++ "p"
+
+fun_4 :: String
+fun_4 = "V" ++ fun_3
+
+fun_5 :: String
+fun_5 = fun_1 ++ "P" ++ fun_4
