052ecce91fd92c6b3266d61d38c0f16ae3580f54
vil02 <vil02@users.noreply.github.com>
2025-05-15 22:47:32 +0000

    feat: add `p_650`

---
+++ src/p_650.hs
@@ -0,0 +1,57 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_17
+
+fun_0 :: String
+fun_0 = "B" ++ "a"
+
+fun_1 :: String
+fun_1 = "U" ++ "O"
+
+fun_2 :: String
+fun_2 = fun_1 ++ "f"
+
+fun_3 :: String
+fun_3 = fun_2 ++ "s"
+
+fun_4 :: String
+fun_4 = "d" ++ "M" ++ "c"
+
+fun_5 :: String
+fun_5 = fun_3 ++ "S" ++ fun_4 ++ "n"
+
+fun_6 :: String
+fun_6 = "j" ++ "N"
+
+fun_7 :: String
+fun_7 = fun_6 ++ "J"
+
+fun_8 :: String
+fun_8 = "n" ++ "l" ++ fun_7
+
+fun_9 :: String
+fun_9 = "7" ++ fun_8
+
+fun_10 :: String
+fun_10 = "u" ++ "l"
+
+fun_11 :: String
+fun_11 = "K" ++ "j"
+
+fun_12 :: String
+fun_12 = "P" ++ fun_11
+
+fun_13 :: String
+fun_13 = "V" ++ "C"
+
+fun_14 :: String
+fun_14 = fun_13 ++ "r"
+
+fun_15 :: String
+fun_15 = fun_12 ++ "7" ++ fun_14 ++ "h"
+
+fun_16 :: String
+fun_16 = fun_15 ++ "Y"
+
+fun_17 :: String
+fun_17 = fun_0 ++ fun_5 ++ fun_9 ++ "6" ++ fun_10 ++ fun_16 ++ "W"
