076bfdaa9e76d092eaeb59f99e371035d2c0fe2b
vil02 <vil02@users.noreply.github.com>
2025-05-17 22:46:42 +0000

    feat: add `p_656`

---
+++ src/p_656.hs
@@ -0,0 +1,57 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_17
+
+fun_0 :: String
+fun_0 = "Y" ++ "k" ++ "1"
+
+fun_1 :: String
+fun_1 = "y" ++ fun_0
+
+fun_2 :: String
+fun_2 = "u" ++ "b" ++ "M" ++ fun_1
+
+fun_3 :: String
+fun_3 = "r" ++ fun_2
+
+fun_4 :: String
+fun_4 = fun_3 ++ "L"
+
+fun_5 :: String
+fun_5 = "2" ++ "B"
+
+fun_6 :: String
+fun_6 = "R" ++ fun_5 ++ "G"
+
+fun_7 :: String
+fun_7 = "E" ++ "K"
+
+fun_8 :: String
+fun_8 = "R" ++ "z"
+
+fun_9 :: String
+fun_9 = "6" ++ fun_8 ++ "Z"
+
+fun_10 :: String
+fun_10 = "u" ++ "s"
+
+fun_11 :: String
+fun_11 = "y" ++ fun_10 ++ "j"
+
+fun_12 :: String
+fun_12 = "B" ++ "i" ++ "3"
+
+fun_13 :: String
+fun_13 = fun_11 ++ fun_12
+
+fun_14 :: String
+fun_14 = "e" ++ "H"
+
+fun_15 :: String
+fun_15 = fun_9 ++ fun_13 ++ fun_14
+
+fun_16 :: String
+fun_16 = fun_7 ++ fun_15 ++ "t"
+
+fun_17 :: String
+fun_17 = fun_4 ++ fun_6 ++ fun_16
