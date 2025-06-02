24b2be01f5f117eaa4cb50d213283ed903f50e72
vil02 <vil02@users.noreply.github.com>
2025-06-02 11:46:57 +0000

    feat: add `p_704`

---
+++ src/p_704.hs
@@ -0,0 +1,39 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_11
+
+fun_0 :: String
+fun_0 = "Y" ++ "R"
+
+fun_1 :: String
+fun_1 = "g" ++ "y"
+
+fun_2 :: String
+fun_2 = "1" ++ "s"
+
+fun_3 :: String
+fun_3 = "L" ++ "G" ++ fun_1 ++ fun_2
+
+fun_4 :: String
+fun_4 = "f" ++ "y"
+
+fun_5 :: String
+fun_5 = "G" ++ "L"
+
+fun_6 :: String
+fun_6 = fun_4 ++ fun_5
+
+fun_7 :: String
+fun_7 = fun_3 ++ fun_6 ++ "q"
+
+fun_8 :: String
+fun_8 = fun_7 ++ "K"
+
+fun_9 :: String
+fun_9 = "k" ++ "6"
+
+fun_10 :: String
+fun_10 = "Z" ++ "D" ++ fun_9
+
+fun_11 :: String
+fun_11 = "R" ++ "2" ++ fun_0 ++ fun_8 ++ "O" ++ fun_10
