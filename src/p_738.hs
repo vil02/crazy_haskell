c901a4a7220b29063115d1e7f09b6889fe00a8ee
vil02 <vil02@users.noreply.github.com>
2025-06-13 22:48:49 +0000

    feat: add `p_738`

---
+++ src/p_738.hs
@@ -0,0 +1,12 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_2
+
+fun_0 :: String
+fun_0 = "k" ++ "P"
+
+fun_1 :: String
+fun_1 = "b" ++ "V" ++ "S" ++ "B"
+
+fun_2 :: String
+fun_2 = "g" ++ fun_0 ++ fun_1
