e7686e4cde09d627619184fc493009987601823e
vil02 <vil02@users.noreply.github.com>
2025-06-25 22:49:42 +0000

    feat: add `p_774`

---
+++ src/p_774.hs
@@ -0,0 +1,57 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_17
+
+fun_0 :: String
+fun_0 = "R" ++ "h"
+
+fun_1 :: String
+fun_1 = "3" ++ "U" ++ "3"
+
+fun_2 :: String
+fun_2 = "R" ++ "3" ++ "y"
+
+fun_3 :: String
+fun_3 = fun_2 ++ "A" ++ "l"
+
+fun_4 :: String
+fun_4 = "H" ++ "0"
+
+fun_5 :: String
+fun_5 = "r" ++ "L"
+
+fun_6 :: String
+fun_6 = fun_4 ++ fun_5
+
+fun_7 :: String
+fun_7 = "D" ++ "x"
+
+fun_8 :: String
+fun_8 = "5" ++ "7"
+
+fun_9 :: String
+fun_9 = "E" ++ fun_8
+
+fun_10 :: String
+fun_10 = "4" ++ fun_9 ++ "B"
+
+fun_11 :: String
+fun_11 = fun_3 ++ fun_6 ++ fun_7 ++ fun_10
+
+fun_12 :: String
+fun_12 = fun_0 ++ fun_1 ++ fun_11 ++ "l"
+
+fun_13 :: String
+fun_13 = "c" ++ "t"
+
+fun_14 :: String
+fun_14 = "F" ++ "b"
+
+fun_15 :: String
+fun_15 = "T" ++ fun_14
+
+fun_16 :: String
+fun_16 = "H" ++ "c" ++ fun_13 ++ fun_15
+
+fun_17 :: String
+fun_17 = fun_12 ++ fun_16
