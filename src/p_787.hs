9586875788e5118a3e6fd61a27b799590f672a77
vil02 <vil02@users.noreply.github.com>
2025-06-30 02:33:53 +0000

    feat: add `p_787`

---
+++ src/p_787.hs
@@ -0,0 +1,21 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_5
+
+fun_0 :: String
+fun_0 = "Z" ++ "T"
+
+fun_1 :: String
+fun_1 = "s" ++ "Y"
+
+fun_2 :: String
+fun_2 = "P" ++ fun_0 ++ fun_1 ++ "7"
+
+fun_3 :: String
+fun_3 = "w" ++ "O"
+
+fun_4 :: String
+fun_4 = "L" ++ fun_3
+
+fun_5 :: String
+fun_5 = fun_2 ++ fun_4 ++ "H"
