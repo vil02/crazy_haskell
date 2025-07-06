bc619c2bc14f9cae2db5d01272f102417ae332b3
vil02 <vil02@users.noreply.github.com>
2025-07-06 02:46:29 +0000

    feat: add `p_805`

---
+++ src/p_805.hs
@@ -0,0 +1,36 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_10
+
+fun_0 :: String
+fun_0 = "Y" ++ "8" ++ "O"
+
+fun_1 :: String
+fun_1 = "1" ++ fun_0
+
+fun_2 :: String
+fun_2 = "h" ++ "a"
+
+fun_3 :: String
+fun_3 = "f" ++ "7"
+
+fun_4 :: String
+fun_4 = "S" ++ "J"
+
+fun_5 :: String
+fun_5 = fun_3 ++ fun_4
+
+fun_6 :: String
+fun_6 = fun_2 ++ fun_5
+
+fun_7 :: String
+fun_7 = "I" ++ "g" ++ "r"
+
+fun_8 :: String
+fun_8 = "V" ++ "r" ++ "A"
+
+fun_9 :: String
+fun_9 = fun_7 ++ "p" ++ fun_8 ++ "h" ++ "5"
+
+fun_10 :: String
+fun_10 = fun_1 ++ fun_6 ++ fun_9 ++ "b"
