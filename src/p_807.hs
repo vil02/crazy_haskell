49b9b9d928e3109cadd3553099c0c3145935e700
vil02 <vil02@users.noreply.github.com>
2025-07-06 22:49:42 +0000

    feat: add `p_807`

---
+++ src/p_807.hs
@@ -0,0 +1,12 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_2
+
+fun_0 :: String
+fun_0 = "k" ++ "o"
+
+fun_1 :: String
+fun_1 = "J" ++ "C"
+
+fun_2 :: String
+fun_2 = "u" ++ "6" ++ fun_0 ++ fun_1
