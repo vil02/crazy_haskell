e1f6e2dca67d7e9cd4ebd88fd14457545e47d01c
vil02 <vil02@users.noreply.github.com>
2025-07-07 11:46:50 +0000

    feat: add `p_809`

---
+++ src/p_809.hs
@@ -0,0 +1,57 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_17
+
+fun_0 :: String
+fun_0 = "T" ++ "c"
+
+fun_1 :: String
+fun_1 = fun_0 ++ "o"
+
+fun_2 :: String
+fun_2 = "K" ++ "v"
+
+fun_3 :: String
+fun_3 = "n" ++ "S"
+
+fun_4 :: String
+fun_4 = "C" ++ fun_3
+
+fun_5 :: String
+fun_5 = "f" ++ "W"
+
+fun_6 :: String
+fun_6 = "Z" ++ "F"
+
+fun_7 :: String
+fun_7 = fun_2 ++ fun_4 ++ fun_5 ++ fun_6
+
+fun_8 :: String
+fun_8 = "g" ++ "t"
+
+fun_9 :: String
+fun_9 = fun_1 ++ fun_7 ++ fun_8
+
+fun_10 :: String
+fun_10 = "Z" ++ "p"
+
+fun_11 :: String
+fun_11 = fun_10 ++ "F"
+
+fun_12 :: String
+fun_12 = "M" ++ "T"
+
+fun_13 :: String
+fun_13 = "i" ++ "p"
+
+fun_14 :: String
+fun_14 = "J" ++ "K"
+
+fun_15 :: String
+fun_15 = fun_13 ++ "q" ++ fun_14
+
+fun_16 :: String
+fun_16 = fun_12 ++ fun_15
+
+fun_17 :: String
+fun_17 = fun_9 ++ "P" ++ "Y" ++ fun_11 ++ fun_16
