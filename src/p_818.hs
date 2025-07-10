ca637060e27264a252be1a66d395d2be7316054f
vil02 <vil02@users.noreply.github.com>
2025-07-10 11:47:21 +0000

    feat: add `p_818`

---
+++ src/p_818.hs
@@ -0,0 +1,45 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_13
+
+fun_0 :: String
+fun_0 = "M" ++ "V" ++ "t"
+
+fun_1 :: String
+fun_1 = "L" ++ "Z"
+
+fun_2 :: String
+fun_2 = fun_1 ++ "A"
+
+fun_3 :: String
+fun_3 = "l" ++ "e"
+
+fun_4 :: String
+fun_4 = fun_2 ++ fun_3
+
+fun_5 :: String
+fun_5 = "d" ++ "P"
+
+fun_6 :: String
+fun_6 = "l" ++ "p"
+
+fun_7 :: String
+fun_7 = fun_6 ++ "y"
+
+fun_8 :: String
+fun_8 = fun_5 ++ "4" ++ "Z" ++ fun_7
+
+fun_9 :: String
+fun_9 = "Q" ++ "l"
+
+fun_10 :: String
+fun_10 = "3" ++ fun_9
+
+fun_11 :: String
+fun_11 = fun_8 ++ fun_10 ++ "x"
+
+fun_12 :: String
+fun_12 = fun_4 ++ "r" ++ fun_11
+
+fun_13 :: String
+fun_13 = fun_0 ++ fun_12
