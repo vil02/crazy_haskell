238979f9a4d5f2d875cd10348a2bac2325b0bf58
vil02 <vil02@users.noreply.github.com>
2025-07-18 02:37:15 +0000

    feat: add `p_841`

---
+++ src/p_841.hs
@@ -0,0 +1,54 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_16
+
+fun_0 :: String
+fun_0 = "j" ++ "A" ++ "j"
+
+fun_1 :: String
+fun_1 = "G" ++ "y"
+
+fun_2 :: String
+fun_2 = "w" ++ "F"
+
+fun_3 :: String
+fun_3 = "m" ++ "1"
+
+fun_4 :: String
+fun_4 = "r" ++ "q"
+
+fun_5 :: String
+fun_5 = "0" ++ fun_3 ++ fun_4 ++ "W"
+
+fun_6 :: String
+fun_6 = "v" ++ "0"
+
+fun_7 :: String
+fun_7 = "F" ++ "9"
+
+fun_8 :: String
+fun_8 = fun_7 ++ "i"
+
+fun_9 :: String
+fun_9 = "8" ++ "3"
+
+fun_10 :: String
+fun_10 = "c" ++ fun_8 ++ fun_9
+
+fun_11 :: String
+fun_11 = fun_6 ++ fun_10
+
+fun_12 :: String
+fun_12 = "I" ++ fun_1 ++ fun_2 ++ fun_5 ++ fun_11 ++ "6" ++ "U"
+
+fun_13 :: String
+fun_13 = "k" ++ fun_0 ++ fun_12
+
+fun_14 :: String
+fun_14 = "T" ++ "N"
+
+fun_15 :: String
+fun_15 = "g" ++ fun_14 ++ "G" ++ "L" ++ "m"
+
+fun_16 :: String
+fun_16 = fun_13 ++ "5" ++ fun_15 ++ "A"
