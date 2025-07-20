e00a1cd3ba002ec4ffa08f2683c31a9646fa0265
vil02 <vil02@users.noreply.github.com>
2025-07-20 22:50:44 +0000

    feat: add `p_849`

---
+++ src/p_849.hs
@@ -0,0 +1,12 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_2
+
+fun_0 :: String
+fun_0 = "d" ++ "i"
+
+fun_1 :: String
+fun_1 = "G" ++ "5"
+
+fun_2 :: String
+fun_2 = fun_0 ++ fun_1
