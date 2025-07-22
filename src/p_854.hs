c214e035e66e30068d7e45d8d5f0d1917af091c1
vil02 <vil02@users.noreply.github.com>
2025-07-22 11:48:22 +0000

    feat: add `p_854`

---
+++ src/p_854.hs
@@ -0,0 +1,42 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_12
+
+fun_0 :: String
+fun_0 = "l" ++ "r" ++ "C"
+
+fun_1 :: String
+fun_1 = "m" ++ "B"
+
+fun_2 :: String
+fun_2 = fun_0 ++ fun_1 ++ "J"
+
+fun_3 :: String
+fun_3 = "9" ++ "l" ++ "e"
+
+fun_4 :: String
+fun_4 = "n" ++ fun_3
+
+fun_5 :: String
+fun_5 = "t" ++ "Y"
+
+fun_6 :: String
+fun_6 = fun_5 ++ "u" ++ "l"
+
+fun_7 :: String
+fun_7 = fun_6 ++ "t"
+
+fun_8 :: String
+fun_8 = "r" ++ "Q"
+
+fun_9 :: String
+fun_9 = fun_8 ++ "k"
+
+fun_10 :: String
+fun_10 = fun_4 ++ fun_7 ++ fun_9
+
+fun_11 :: String
+fun_11 = "B" ++ "I" ++ "R"
+
+fun_12 :: String
+fun_12 = fun_2 ++ "I" ++ fun_10 ++ fun_11
