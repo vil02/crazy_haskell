a65b52af15e92d2080be5ef06993ee7163c41b32
vil02 <vil02@users.noreply.github.com>
2025-07-23 22:51:58 +0000

    feat: add `p_858`

---
+++ src/p_858.hs
@@ -0,0 +1,57 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_17
+
+fun_0 :: String
+fun_0 = "a" ++ "i"
+
+fun_1 :: String
+fun_1 = "z" ++ "N"
+
+fun_2 :: String
+fun_2 = fun_1 ++ "4" ++ "I"
+
+fun_3 :: String
+fun_3 = "3" ++ "e"
+
+fun_4 :: String
+fun_4 = fun_3 ++ "4"
+
+fun_5 :: String
+fun_5 = "n" ++ "D"
+
+fun_6 :: String
+fun_6 = fun_4 ++ fun_5
+
+fun_7 :: String
+fun_7 = "e" ++ "Z"
+
+fun_8 :: String
+fun_8 = fun_2 ++ fun_6 ++ fun_7
+
+fun_9 :: String
+fun_9 = "q" ++ "8"
+
+fun_10 :: String
+fun_10 = "u" ++ fun_9 ++ "Y"
+
+fun_11 :: String
+fun_11 = "y" ++ "Z"
+
+fun_12 :: String
+fun_12 = fun_10 ++ fun_11 ++ "G"
+
+fun_13 :: String
+fun_13 = fun_12 ++ "u"
+
+fun_14 :: String
+fun_14 = fun_0 ++ fun_8 ++ fun_13 ++ "m"
+
+fun_15 :: String
+fun_15 = "D" ++ "A"
+
+fun_16 :: String
+fun_16 = "R" ++ "n"
+
+fun_17 :: String
+fun_17 = "e" ++ fun_14 ++ fun_15 ++ "6" ++ fun_16
