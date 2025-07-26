beb4127efa747dc050f7731ceccf8ebd13b14976
vil02 <vil02@users.noreply.github.com>
2025-07-26 11:46:55 +0000

    feat: add `p_866`

---
+++ src/p_866.hs
@@ -0,0 +1,27 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_7
+
+fun_0 :: String
+fun_0 = "K" ++ "H" ++ "Z"
+
+fun_1 :: String
+fun_1 = "U" ++ "K"
+
+fun_2 :: String
+fun_2 = "w" ++ "W"
+
+fun_3 :: String
+fun_3 = fun_1 ++ fun_2
+
+fun_4 :: String
+fun_4 = "s" ++ "7"
+
+fun_5 :: String
+fun_5 = "k" ++ fun_3 ++ "5" ++ fun_4
+
+fun_6 :: String
+fun_6 = fun_5 ++ "H" ++ "W"
+
+fun_7 :: String
+fun_7 = fun_0 ++ fun_6
