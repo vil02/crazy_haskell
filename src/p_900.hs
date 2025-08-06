edc7ce585d906aa8041087ed035faa9cdd316f18
vil02 <vil02@users.noreply.github.com>
2025-08-06 22:52:44 +0000

    feat: add `p_900`

---
+++ src/p_900.hs
@@ -0,0 +1,12 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_2
+
+fun_0 :: String
+fun_0 = "L" ++ "O" ++ "3"
+
+fun_1 :: String
+fun_1 = "d" ++ "l" ++ "S"
+
+fun_2 :: String
+fun_2 = fun_0 ++ "E" ++ fun_1
