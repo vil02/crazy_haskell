3f14d039b0581bdf4dd5a14153bd54b436637ce0
vil02 <vil02@users.noreply.github.com>
2025-08-14 11:47:33 +0000

    feat: add `p_923`

---
+++ src/p_923.hs
@@ -0,0 +1,54 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_16
+
+fun_0 :: String
+fun_0 = "6" ++ "A"
+
+fun_1 :: String
+fun_1 = "J" ++ "S"
+
+fun_2 :: String
+fun_2 = "Y" ++ "G"
+
+fun_3 :: String
+fun_3 = "a" ++ "O" ++ "9" ++ fun_2
+
+fun_4 :: String
+fun_4 = fun_3 ++ "T"
+
+fun_5 :: String
+fun_5 = "e" ++ "m"
+
+fun_6 :: String
+fun_6 = "u" ++ "z"
+
+fun_7 :: String
+fun_7 = fun_5 ++ "I" ++ fun_6 ++ "P"
+
+fun_8 :: String
+fun_8 = fun_1 ++ fun_4 ++ fun_7 ++ "T" ++ "q"
+
+fun_9 :: String
+fun_9 = "n" ++ "L"
+
+fun_10 :: String
+fun_10 = "K" ++ "h"
+
+fun_11 :: String
+fun_11 = "i" ++ fun_10
+
+fun_12 :: String
+fun_12 = "Q" ++ fun_11
+
+fun_13 :: String
+fun_13 = "7" ++ "K"
+
+fun_14 :: String
+fun_14 = "J" ++ "O"
+
+fun_15 :: String
+fun_15 = fun_13 ++ fun_14
+
+fun_16 :: String
+fun_16 = fun_0 ++ fun_8 ++ fun_9 ++ fun_12 ++ "5" ++ fun_15 ++ "J"
