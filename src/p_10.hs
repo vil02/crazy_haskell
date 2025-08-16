beaf3b2c0f31f2e7fb3ec3affe94f3093088279a
vil02 <vil02@users.noreply.github.com>
2025-08-16 11:46:40 +0000

    feat: add `p_10`

---
+++ src/p_10.hs
@@ -0,0 +1,24 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_6
+
+fun_0 :: String
+fun_0 = "U" ++ "E"
+
+fun_1 :: String
+fun_1 = fun_0 ++ "d"
+
+fun_2 :: String
+fun_2 = "8" ++ fun_1
+
+fun_3 :: String
+fun_3 = "N" ++ fun_2
+
+fun_4 :: String
+fun_4 = "o" ++ "e"
+
+fun_5 :: String
+fun_5 = fun_4 ++ "L"
+
+fun_6 :: String
+fun_6 = fun_3 ++ "D" ++ fun_5
