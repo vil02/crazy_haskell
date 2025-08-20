beb5145a21e0ce5d33afe1c5eb1a53744c70b877
vil02 <vil02@users.noreply.github.com>
2025-08-20 22:46:43 +0000

    feat: add `p_23`

---
+++ src/p_23.hs
@@ -0,0 +1,33 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_9
+
+fun_0 :: String
+fun_0 = "g" ++ "v"
+
+fun_1 :: String
+fun_1 = fun_0 ++ "0"
+
+fun_2 :: String
+fun_2 = "O" ++ "q"
+
+fun_3 :: String
+fun_3 = "u" ++ "b" ++ "r"
+
+fun_4 :: String
+fun_4 = "C" ++ "4"
+
+fun_5 :: String
+fun_5 = fun_4 ++ "5"
+
+fun_6 :: String
+fun_6 = fun_3 ++ fun_5
+
+fun_7 :: String
+fun_7 = "7" ++ "q"
+
+fun_8 :: String
+fun_8 = fun_7 ++ "G"
+
+fun_9 :: String
+fun_9 = fun_1 ++ fun_2 ++ fun_6 ++ fun_8
