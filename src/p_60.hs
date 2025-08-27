9b0e9a25437300e1013cb578912c112a67f27511
vil02 <vil02@users.noreply.github.com>
2025-08-27 02:15:16 +0000

    feat: add `p_60`

---
+++ src/p_60.hs
@@ -0,0 +1,30 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_8
+
+fun_0 :: String
+fun_0 = "o" ++ "Y"
+
+fun_1 :: String
+fun_1 = "t" ++ fun_0
+
+fun_2 :: String
+fun_2 = fun_1 ++ "k" ++ "b"
+
+fun_3 :: String
+fun_3 = "2" ++ "3"
+
+fun_4 :: String
+fun_4 = "t" ++ "g"
+
+fun_5 :: String
+fun_5 = "b" ++ "m"
+
+fun_6 :: String
+fun_6 = fun_4 ++ fun_5 ++ "h"
+
+fun_7 :: String
+fun_7 = "2" ++ "8" ++ "9"
+
+fun_8 :: String
+fun_8 = "V" ++ fun_2 ++ fun_3 ++ fun_6 ++ fun_7
