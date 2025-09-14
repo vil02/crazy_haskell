ab807e6d296edb4164f28611f9c00b5b5ffdb2ec
vil02 <vil02@users.noreply.github.com>
2025-09-14 11:46:47 +0000

    feat: add `p_127`

---
+++ src/p_127.hs
@@ -0,0 +1,18 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_4
+
+fun_0 :: String
+fun_0 = "L" ++ "p"
+
+fun_1 :: String
+fun_1 = "e" ++ "X"
+
+fun_2 :: String
+fun_2 = fun_1 ++ "x"
+
+fun_3 :: String
+fun_3 = "d" ++ fun_2 ++ "l"
+
+fun_4 :: String
+fun_4 = fun_0 ++ "O" ++ fun_3 ++ "3"
