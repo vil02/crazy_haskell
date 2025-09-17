64705a6b9d930038710180f5f33be8e37c14242f
vil02 <vil02@users.noreply.github.com>
2025-09-17 02:08:56 +0000

    feat: add `p_135`

---
+++ src/p_135.hs
@@ -0,0 +1,48 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_14
+
+fun_0 :: String
+fun_0 = "z" ++ "A"
+
+fun_1 :: String
+fun_1 = "P" ++ "G" ++ "X"
+
+fun_2 :: String
+fun_2 = fun_1 ++ "e"
+
+fun_3 :: String
+fun_3 = "p" ++ "m"
+
+fun_4 :: String
+fun_4 = "0" ++ fun_2 ++ fun_3
+
+fun_5 :: String
+fun_5 = fun_0 ++ fun_4
+
+fun_6 :: String
+fun_6 = fun_5 ++ "n"
+
+fun_7 :: String
+fun_7 = "l" ++ "A"
+
+fun_8 :: String
+fun_8 = "s" ++ "m"
+
+fun_9 :: String
+fun_9 = "5" ++ "Y"
+
+fun_10 :: String
+fun_10 = "i" ++ fun_9
+
+fun_11 :: String
+fun_11 = "K" ++ "7"
+
+fun_12 :: String
+fun_12 = fun_7 ++ fun_8 ++ fun_10 ++ fun_11
+
+fun_13 :: String
+fun_13 = "N" ++ "v" ++ "N" ++ fun_12
+
+fun_14 :: String
+fun_14 = "i" ++ fun_6 ++ fun_13 ++ "D"
