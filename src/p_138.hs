b35361d14ef174c2665c8c9f4c19696eae84af54
vil02 <vil02@users.noreply.github.com>
2025-09-18 02:09:17 +0000

    feat: add `p_138`

---
+++ src/p_138.hs
@@ -0,0 +1,48 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_14
+
+fun_0 :: String
+fun_0 = "a" ++ "0"
+
+fun_1 :: String
+fun_1 = "i" ++ fun_0
+
+fun_2 :: String
+fun_2 = "R" ++ fun_1 ++ "O" ++ "m"
+
+fun_3 :: String
+fun_3 = "d" ++ "7"
+
+fun_4 :: String
+fun_4 = "a" ++ fun_3 ++ "x"
+
+fun_5 :: String
+fun_5 = "6" ++ "x" ++ "u"
+
+fun_6 :: String
+fun_6 = "z" ++ "4" ++ fun_4 ++ fun_5 ++ "B"
+
+fun_7 :: String
+fun_7 = "d" ++ fun_2 ++ fun_6
+
+fun_8 :: String
+fun_8 = "F" ++ "s"
+
+fun_9 :: String
+fun_9 = "j" ++ "7"
+
+fun_10 :: String
+fun_10 = "i" ++ fun_8 ++ fun_9
+
+fun_11 :: String
+fun_11 = "V" ++ "C"
+
+fun_12 :: String
+fun_12 = "V" ++ fun_11 ++ "7"
+
+fun_13 :: String
+fun_13 = "m" ++ fun_10 ++ "t" ++ fun_12
+
+fun_14 :: String
+fun_14 = fun_7 ++ fun_13
