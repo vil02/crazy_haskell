b029390c653066995522e13151464865324fd283
vil02 <vil02@users.noreply.github.com>
2025-09-20 02:07:06 +0000

    feat: add `p_144`

---
+++ src/p_144.hs
@@ -0,0 +1,12 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_2
+
+fun_0 :: String
+fun_0 = "o" ++ "L" ++ "s"
+
+fun_1 :: String
+fun_1 = "k" ++ "A"
+
+fun_2 :: String
+fun_2 = fun_0 ++ "4" ++ fun_1
