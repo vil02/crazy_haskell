2b885344ce5b1529b91471139ba43ee59945780b
vil02 <vil02@users.noreply.github.com>
2025-09-26 11:46:47 +0000

    feat: add `p_163`

---
+++ src/p_163.hs
@@ -0,0 +1,18 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_4
+
+fun_0 :: String
+fun_0 = "z" ++ "7" ++ "S"
+
+fun_1 :: String
+fun_1 = "b" ++ "1"
+
+fun_2 :: String
+fun_2 = "R" ++ "q"
+
+fun_3 :: String
+fun_3 = fun_1 ++ fun_2 ++ "g"
+
+fun_4 :: String
+fun_4 = fun_0 ++ fun_3
