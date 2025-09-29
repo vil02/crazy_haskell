ffa27f657cb01d3141ec9db3fb75e55e7d612240
vil02 <vil02@users.noreply.github.com>
2025-09-29 11:46:53 +0000

    feat: add `p_172`

---
+++ src/p_172.hs
@@ -0,0 +1,54 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_16
+
+fun_0 :: String
+fun_0 = "a" ++ "3" ++ "d"
+
+fun_1 :: String
+fun_1 = fun_0 ++ "p"
+
+fun_2 :: String
+fun_2 = "U" ++ fun_1
+
+fun_3 :: String
+fun_3 = "r" ++ "M"
+
+fun_4 :: String
+fun_4 = "q" ++ fun_3
+
+fun_5 :: String
+fun_5 = "R" ++ "n" ++ fun_4
+
+fun_6 :: String
+fun_6 = "h" ++ "S"
+
+fun_7 :: String
+fun_7 = "Z" ++ "C"
+
+fun_8 :: String
+fun_8 = fun_7 ++ "R"
+
+fun_9 :: String
+fun_9 = "w" ++ fun_8
+
+fun_10 :: String
+fun_10 = "I" ++ fun_9 ++ "3" ++ "C"
+
+fun_11 :: String
+fun_11 = "d" ++ "E" ++ fun_6 ++ fun_10
+
+fun_12 :: String
+fun_12 = fun_2 ++ "g" ++ "1" ++ fun_5 ++ fun_11 ++ "n"
+
+fun_13 :: String
+fun_13 = "z" ++ "H"
+
+fun_14 :: String
+fun_14 = "i" ++ "c"
+
+fun_15 :: String
+fun_15 = fun_14 ++ "h"
+
+fun_16 :: String
+fun_16 = "L" ++ "q" ++ fun_12 ++ fun_13 ++ fun_15 ++ "7" ++ "t"
