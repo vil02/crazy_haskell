fb90f156d0f3763a3eac42ccd9bf361a8c94deb4
vil02 <vil02@users.noreply.github.com>
2025-09-30 11:46:40 +0000

    feat: add `p_175`

---
+++ src/p_175.hs
@@ -0,0 +1,33 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_9
+
+fun_0 :: String
+fun_0 = "S" ++ "y" ++ "0"
+
+fun_1 :: String
+fun_1 = "i" ++ "N"
+
+fun_2 :: String
+fun_2 = fun_0 ++ fun_1 ++ "G"
+
+fun_3 :: String
+fun_3 = "N" ++ "l"
+
+fun_4 :: String
+fun_4 = "f" ++ "f" ++ fun_2 ++ fun_3
+
+fun_5 :: String
+fun_5 = "0" ++ "f"
+
+fun_6 :: String
+fun_6 = "X" ++ "3"
+
+fun_7 :: String
+fun_7 = fun_5 ++ fun_6
+
+fun_8 :: String
+fun_8 = "w" ++ fun_7
+
+fun_9 :: String
+fun_9 = fun_4 ++ "b" ++ fun_8
