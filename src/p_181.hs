2444689556907a61c336147b535bc71070f4ba02
vil02 <vil02@users.noreply.github.com>
2025-10-02 11:46:44 +0000

    feat: add `p_181`

---
+++ src/p_181.hs
@@ -0,0 +1,39 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_11
+
+fun_0 :: String
+fun_0 = "c" ++ "p" ++ "2" ++ "X"
+
+fun_1 :: String
+fun_1 = "B" ++ "V"
+
+fun_2 :: String
+fun_2 = fun_0 ++ fun_1
+
+fun_3 :: String
+fun_3 = "1" ++ "G"
+
+fun_4 :: String
+fun_4 = "y" ++ "c"
+
+fun_5 :: String
+fun_5 = "0" ++ "2"
+
+fun_6 :: String
+fun_6 = "E" ++ fun_5
+
+fun_7 :: String
+fun_7 = fun_4 ++ "q" ++ fun_6
+
+fun_8 :: String
+fun_8 = "e" ++ fun_3 ++ fun_7
+
+fun_9 :: String
+fun_9 = fun_2 ++ "3" ++ fun_8
+
+fun_10 :: String
+fun_10 = "5" ++ fun_9
+
+fun_11 :: String
+fun_11 = "t" ++ fun_10 ++ "B"
