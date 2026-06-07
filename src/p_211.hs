e017948b55d6caff94f78fed5cfd329ea498e829
vil02 <vil02@users.noreply.github.com>
2026-06-07 23:43:30 +0000

    feat: add `p_211`

---
+++ src/p_211.hs
@@ -0,0 +1,21 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_5
+
+fun_0 :: String
+fun_0 = "z" ++ "I"
+
+fun_1 :: String
+fun_1 = "t" ++ "Z"
+
+fun_2 :: String
+fun_2 = "r" ++ "v" ++ "G" ++ fun_1
+
+fun_3 :: String
+fun_3 = "2" ++ "n"
+
+fun_4 :: String
+fun_4 = fun_3 ++ "D"
+
+fun_5 :: String
+fun_5 = "U" ++ fun_0 ++ fun_2 ++ fun_4
