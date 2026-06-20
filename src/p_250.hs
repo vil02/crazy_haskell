7436c80bb93d599b27cc64a5d5c21769b76f1448
vil02 <vil02@users.noreply.github.com>
2026-06-20 23:48:57 +0000

    feat: add `p_250`

---
+++ src/p_250.hs
@@ -0,0 +1,54 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_16
+
+fun_0 :: String
+fun_0 = "R" ++ "o" ++ "p"
+
+fun_1 :: String
+fun_1 = fun_0 ++ "C"
+
+fun_2 :: String
+fun_2 = fun_1 ++ "u"
+
+fun_3 :: String
+fun_3 = "M" ++ fun_2 ++ "N"
+
+fun_4 :: String
+fun_4 = "p" ++ "C"
+
+fun_5 :: String
+fun_5 = "x" ++ "B"
+
+fun_6 :: String
+fun_6 = "z" ++ "k" ++ fun_5
+
+fun_7 :: String
+fun_7 = fun_4 ++ fun_6
+
+fun_8 :: String
+fun_8 = "L" ++ "G" ++ fun_7
+
+fun_9 :: String
+fun_9 = "K" ++ fun_3 ++ fun_8
+
+fun_10 :: String
+fun_10 = "Y" ++ "x"
+
+fun_11 :: String
+fun_11 = "f" ++ "L"
+
+fun_12 :: String
+fun_12 = "b" ++ fun_11
+
+fun_13 :: String
+fun_13 = "V" ++ "S"
+
+fun_14 :: String
+fun_14 = "v" ++ fun_13
+
+fun_15 :: String
+fun_15 = fun_10 ++ "x" ++ "X" ++ fun_12 ++ fun_14
+
+fun_16 :: String
+fun_16 = fun_9 ++ "C" ++ fun_15 ++ "O" ++ "y" ++ "G"
