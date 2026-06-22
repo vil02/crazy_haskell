99ef367e79e38df531e25a473e70e4662ab8977e
vil02 <vil02@users.noreply.github.com>
2026-06-22 16:17:12 +0000

    feat: add `p_255`

---
+++ src/p_255.hs
@@ -0,0 +1,39 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_11
+
+fun_0 :: String
+fun_0 = "q" ++ "A"
+
+fun_1 :: String
+fun_1 = "M" ++ fun_0
+
+fun_2 :: String
+fun_2 = "k" ++ "E"
+
+fun_3 :: String
+fun_3 = fun_2 ++ "y"
+
+fun_4 :: String
+fun_4 = fun_3 ++ "T"
+
+fun_5 :: String
+fun_5 = fun_4 ++ "8"
+
+fun_6 :: String
+fun_6 = fun_5 ++ "H"
+
+fun_7 :: String
+fun_7 = "w" ++ fun_6 ++ "P" ++ "9"
+
+fun_8 :: String
+fun_8 = "2" ++ "n"
+
+fun_9 :: String
+fun_9 = fun_8 ++ "N"
+
+fun_10 :: String
+fun_10 = fun_1 ++ "q" ++ fun_7 ++ fun_9
+
+fun_11 :: String
+fun_11 = "z" ++ fun_10
