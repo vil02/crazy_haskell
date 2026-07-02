facca6b4a2880e5db7bc2528a6540209e1b305d5
vil02 <vil02@users.noreply.github.com>
2026-07-02 13:21:12 +0000

    feat: add `p_285`

---
+++ src/p_285.hs
@@ -0,0 +1,27 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_7
+
+fun_0 :: String
+fun_0 = "R" ++ "w"
+
+fun_1 :: String
+fun_1 = fun_0 ++ "l"
+
+fun_2 :: String
+fun_2 = fun_1 ++ "k"
+
+fun_3 :: String
+fun_3 = fun_2 ++ "D"
+
+fun_4 :: String
+fun_4 = "3" ++ "w"
+
+fun_5 :: String
+fun_5 = fun_4 ++ "K"
+
+fun_6 :: String
+fun_6 = "J" ++ "y"
+
+fun_7 :: String
+fun_7 = fun_3 ++ fun_5 ++ "f" ++ fun_6 ++ "g"
