8cc8a0163c39966170a2239ef49c7f309756e0fe
vil02 <vil02@users.noreply.github.com>
2026-07-08 23:43:38 +0000

    feat: add `p_310`

---
+++ src/p_310.hs
@@ -0,0 +1,57 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_17
+
+fun_0 :: String
+fun_0 = "4" ++ "7"
+
+fun_1 :: String
+fun_1 = "i" ++ "x"
+
+fun_2 :: String
+fun_2 = fun_1 ++ "h"
+
+fun_3 :: String
+fun_3 = "2" ++ fun_0 ++ fun_2
+
+fun_4 :: String
+fun_4 = "c" ++ "l" ++ "x"
+
+fun_5 :: String
+fun_5 = fun_3 ++ fun_4
+
+fun_6 :: String
+fun_6 = "u" ++ "z"
+
+fun_7 :: String
+fun_7 = "J" ++ "y"
+
+fun_8 :: String
+fun_8 = "y" ++ fun_7
+
+fun_9 :: String
+fun_9 = "y" ++ "D"
+
+fun_10 :: String
+fun_10 = fun_8 ++ fun_9 ++ "H"
+
+fun_11 :: String
+fun_11 = "7" ++ "e"
+
+fun_12 :: String
+fun_12 = fun_10 ++ fun_11 ++ "x"
+
+fun_13 :: String
+fun_13 = "c" ++ "A"
+
+fun_14 :: String
+fun_14 = fun_13 ++ "Q" ++ "L"
+
+fun_15 :: String
+fun_15 = "c" ++ "p"
+
+fun_16 :: String
+fun_16 = fun_14 ++ "F" ++ "s" ++ fun_15
+
+fun_17 :: String
+fun_17 = fun_5 ++ fun_6 ++ fun_12 ++ fun_16
