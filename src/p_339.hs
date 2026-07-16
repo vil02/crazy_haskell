02a8fffba4337a66a81e15445f929f66ace30809
vil02 <vil02@users.noreply.github.com>
2026-07-16 13:05:28 +0000

    feat: add `p_339`

---
+++ src/p_339.hs
@@ -0,0 +1,57 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_17
+
+fun_0 :: String
+fun_0 = "w" ++ "P"
+
+fun_1 :: String
+fun_1 = fun_0 ++ "r"
+
+fun_2 :: String
+fun_2 = "5" ++ "A" ++ "5"
+
+fun_3 :: String
+fun_3 = "7" ++ "G"
+
+fun_4 :: String
+fun_4 = fun_1 ++ "b" ++ fun_2 ++ fun_3
+
+fun_5 :: String
+fun_5 = "t" ++ "X"
+
+fun_6 :: String
+fun_6 = "r" ++ fun_5
+
+fun_7 :: String
+fun_7 = "2" ++ "W"
+
+fun_8 :: String
+fun_8 = "7" ++ "j" ++ fun_7
+
+fun_9 :: String
+fun_9 = "S" ++ "u"
+
+fun_10 :: String
+fun_10 = "4" ++ "C"
+
+fun_11 :: String
+fun_11 = "T" ++ fun_10
+
+fun_12 :: String
+fun_12 = fun_9 ++ "t" ++ fun_11
+
+fun_13 :: String
+fun_13 = fun_8 ++ "t" ++ fun_12
+
+fun_14 :: String
+fun_14 = "n" ++ "4"
+
+fun_15 :: String
+fun_15 = "7" ++ "g"
+
+fun_16 :: String
+fun_16 = fun_15 ++ "R"
+
+fun_17 :: String
+fun_17 = fun_4 ++ fun_6 ++ fun_13 ++ "Z" ++ fun_14 ++ fun_16
