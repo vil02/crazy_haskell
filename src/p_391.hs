f92d4cebcd594d67cdbaa6b60935cbbdcec122dd
vil02 <vil02@users.noreply.github.com>
2026-07-23 23:36:46 +0000

    feat: add `p_391`

---
+++ src/p_391.hs
@@ -0,0 +1,57 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_17
+
+fun_0 :: String
+fun_0 = "E" ++ "i"
+
+fun_1 :: String
+fun_1 = "C" ++ fun_0
+
+fun_2 :: String
+fun_2 = "9" ++ "r"
+
+fun_3 :: String
+fun_3 = "V" ++ fun_2 ++ "m"
+
+fun_4 :: String
+fun_4 = "4" ++ "S" ++ fun_3
+
+fun_5 :: String
+fun_5 = "G" ++ "d"
+
+fun_6 :: String
+fun_6 = "U" ++ "f" ++ fun_5
+
+fun_7 :: String
+fun_7 = "A" ++ "B"
+
+fun_8 :: String
+fun_8 = "x" ++ "M" ++ "c"
+
+fun_9 :: String
+fun_9 = "z" ++ "P"
+
+fun_10 :: String
+fun_10 = "X" ++ fun_9
+
+fun_11 :: String
+fun_11 = fun_8 ++ fun_10
+
+fun_12 :: String
+fun_12 = "e" ++ fun_11
+
+fun_13 :: String
+fun_13 = "V" ++ "5"
+
+fun_14 :: String
+fun_14 = fun_12 ++ fun_13
+
+fun_15 :: String
+fun_15 = fun_7 ++ "M" ++ fun_14
+
+fun_16 :: String
+fun_16 = fun_4 ++ fun_6 ++ fun_15
+
+fun_17 :: String
+fun_17 = fun_1 ++ fun_16 ++ "J"
