df72c95d843df24e18508a58b0e67af054c373ec
vil02 <vil02@users.noreply.github.com>
2026-07-24 13:05:28 +0000

    feat: add `p_393`

---
+++ src/p_393.hs
@@ -0,0 +1,30 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_8
+
+fun_0 :: String
+fun_0 = "r" ++ "m"
+
+fun_1 :: String
+fun_1 = fun_0 ++ "g" ++ "W"
+
+fun_2 :: String
+fun_2 = "Y" ++ "f"
+
+fun_3 :: String
+fun_3 = "m" ++ fun_2
+
+fun_4 :: String
+fun_4 = "9" ++ "3"
+
+fun_5 :: String
+fun_5 = fun_4 ++ "v"
+
+fun_6 :: String
+fun_6 = "u" ++ "E"
+
+fun_7 :: String
+fun_7 = "W" ++ fun_5 ++ fun_6 ++ "L" ++ "i"
+
+fun_8 :: String
+fun_8 = fun_1 ++ fun_3 ++ "y" ++ fun_7 ++ "S" ++ "m"
