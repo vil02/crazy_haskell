27f8aaa257b96ff0a7bcc8fea59208a5e692ee9b
vil02 <vil02@users.noreply.github.com>
2026-07-26 23:40:08 +0000

    feat: add `p_401`

---
+++ src/p_401.hs
@@ -0,0 +1,36 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_10
+
+fun_0 :: String
+fun_0 = "i" ++ "u" ++ "9" ++ "F"
+
+fun_1 :: String
+fun_1 = "y" ++ "2"
+
+fun_2 :: String
+fun_2 = "9" ++ "P" ++ "F"
+
+fun_3 :: String
+fun_3 = "n" ++ "r"
+
+fun_4 :: String
+fun_4 = "A" ++ "k"
+
+fun_5 :: String
+fun_5 = fun_2 ++ fun_3 ++ fun_4 ++ "F"
+
+fun_6 :: String
+fun_6 = "2" ++ fun_5
+
+fun_7 :: String
+fun_7 = "g" ++ "N" ++ "M"
+
+fun_8 :: String
+fun_8 = fun_7 ++ "W" ++ "o"
+
+fun_9 :: String
+fun_9 = "G" ++ "n" ++ "N"
+
+fun_10 :: String
+fun_10 = fun_0 ++ "5" ++ "w" ++ fun_1 ++ fun_6 ++ fun_8 ++ fun_9
