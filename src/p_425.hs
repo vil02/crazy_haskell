df241f488d4ea3bee850ac74bbe449786102a7c8
vil02 <vil02@users.noreply.github.com>
2026-08-02 04:16:52 +0000

    feat: add `p_425`

---
+++ src/p_425.hs
@@ -0,0 +1,12 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_2
+
+fun_0 :: String
+fun_0 = "E" ++ "h"
+
+fun_1 :: String
+fun_1 = fun_0 ++ "n"
+
+fun_2 :: String
+fun_2 = "4" ++ fun_1
