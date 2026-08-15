e44fb1a9c8607d98be840bab7a24ee7c7cd225e4
vil02 <vil02@users.noreply.github.com>
2026-08-15 22:57:03 +0000

    feat: add `p_502`

---
+++ src/p_502.hs
@@ -0,0 +1,30 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_8
+
+fun_0 :: String
+fun_0 = "s" ++ "G" ++ "k"
+
+fun_1 :: String
+fun_1 = "w" ++ "7" ++ "Z"
+
+fun_2 :: String
+fun_2 = fun_1 ++ "2"
+
+fun_3 :: String
+fun_3 = "5" ++ "w"
+
+fun_4 :: String
+fun_4 = "o" ++ fun_3
+
+fun_5 :: String
+fun_5 = fun_4 ++ "t"
+
+fun_6 :: String
+fun_6 = "N" ++ fun_5 ++ "B"
+
+fun_7 :: String
+fun_7 = fun_0 ++ fun_2 ++ fun_6
+
+fun_8 :: String
+fun_8 = "3" ++ fun_7
