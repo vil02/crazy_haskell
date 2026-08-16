952dda07445e603c9ad7037691b380db87f0edcc
vil02 <vil02@users.noreply.github.com>
2026-08-16 22:56:37 +0000

    feat: add `p_510`

---
+++ src/p_510.hs
@@ -0,0 +1,60 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_18
+
+fun_0 :: String
+fun_0 = "Q" ++ "X"
+
+fun_1 :: String
+fun_1 = "E" ++ "G"
+
+fun_2 :: String
+fun_2 = "K" ++ "f"
+
+fun_3 :: String
+fun_3 = fun_1 ++ fun_2
+
+fun_4 :: String
+fun_4 = "0" ++ "P"
+
+fun_5 :: String
+fun_5 = fun_4 ++ "L"
+
+fun_6 :: String
+fun_6 = fun_3 ++ fun_5
+
+fun_7 :: String
+fun_7 = "J" ++ "w"
+
+fun_8 :: String
+fun_8 = fun_7 ++ "U"
+
+fun_9 :: String
+fun_9 = "9" ++ "S"
+
+fun_10 :: String
+fun_10 = fun_8 ++ fun_9 ++ "Q" ++ "a"
+
+fun_11 :: String
+fun_11 = fun_6 ++ fun_10 ++ "x"
+
+fun_12 :: String
+fun_12 = "L" ++ "d"
+
+fun_13 :: String
+fun_13 = fun_12 ++ "X"
+
+fun_14 :: String
+fun_14 = fun_13 ++ "p"
+
+fun_15 :: String
+fun_15 = "1" ++ "n"
+
+fun_16 :: String
+fun_16 = fun_15 ++ "A"
+
+fun_17 :: String
+fun_17 = "y" ++ fun_14 ++ fun_16
+
+fun_18 :: String
+fun_18 = fun_0 ++ fun_11 ++ fun_17
