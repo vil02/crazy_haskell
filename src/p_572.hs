f3d85bc5f6b9a8a3c978cfd8d779d172ec89c049
vil02 <vil02@users.noreply.github.com>
2026-08-30 05:52:24 +0000

    feat: add `p_572`

---
+++ src/p_572.hs
@@ -0,0 +1,12 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_2
+
+fun_0 :: String
+fun_0 = "1" ++ "h" ++ "Q"
+
+fun_1 :: String
+fun_1 = "x" ++ fun_0 ++ "G"
+
+fun_2 :: String
+fun_2 = fun_1 ++ "v"
