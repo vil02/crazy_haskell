b311feca0fc90e2e9212ae35aadad3efc8a85710
vil02 <vil02@users.noreply.github.com>
2026-09-07 00:20:01 +0000

    feat: add `p_604`

---
+++ src/p_604.hs
@@ -0,0 +1,57 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_17
+
+fun_0 :: String
+fun_0 = "v" ++ "X"
+
+fun_1 :: String
+fun_1 = fun_0 ++ "C"
+
+fun_2 :: String
+fun_2 = "3" ++ "T"
+
+fun_3 :: String
+fun_3 = fun_1 ++ fun_2
+
+fun_4 :: String
+fun_4 = "B" ++ "P"
+
+fun_5 :: String
+fun_5 = "v" ++ "Z"
+
+fun_6 :: String
+fun_6 = fun_5 ++ "1"
+
+fun_7 :: String
+fun_7 = "9" ++ "U" ++ "R"
+
+fun_8 :: String
+fun_8 = "R" ++ fun_7 ++ "O"
+
+fun_9 :: String
+fun_9 = "7" ++ "O"
+
+fun_10 :: String
+fun_10 = fun_9 ++ "h"
+
+fun_11 :: String
+fun_11 = fun_8 ++ "N" ++ fun_10
+
+fun_12 :: String
+fun_12 = "w" ++ fun_11
+
+fun_13 :: String
+fun_13 = "D" ++ "2" ++ fun_12
+
+fun_14 :: String
+fun_14 = fun_13 ++ "j"
+
+fun_15 :: String
+fun_15 = "C" ++ fun_4 ++ fun_6 ++ fun_14
+
+fun_16 :: String
+fun_16 = "J" ++ "d" ++ "C"
+
+fun_17 :: String
+fun_17 = fun_3 ++ fun_15 ++ fun_16
