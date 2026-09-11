1f04db95c7e7fa22ad4f0b315dc7db333584c2bc
vil02 <vil02@users.noreply.github.com>
2026-09-11 00:32:52 +0000

    feat: add `p_616`

---
+++ src/p_616.hs
@@ -0,0 +1,54 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_16
+
+fun_0 :: String
+fun_0 = "P" ++ "k"
+
+fun_1 :: String
+fun_1 = "f" ++ "o"
+
+fun_2 :: String
+fun_2 = "U" ++ "6"
+
+fun_3 :: String
+fun_3 = fun_1 ++ "n" ++ fun_2
+
+fun_4 :: String
+fun_4 = "s" ++ "Z"
+
+fun_5 :: String
+fun_5 = "z" ++ "w"
+
+fun_6 :: String
+fun_6 = "w" ++ "X" ++ "Z"
+
+fun_7 :: String
+fun_7 = fun_6 ++ "W"
+
+fun_8 :: String
+fun_8 = fun_7 ++ "y"
+
+fun_9 :: String
+fun_9 = "E" ++ "6"
+
+fun_10 :: String
+fun_10 = "d" ++ "R" ++ fun_9
+
+fun_11 :: String
+fun_11 = fun_8 ++ fun_10 ++ "5"
+
+fun_12 :: String
+fun_12 = "z" ++ "g"
+
+fun_13 :: String
+fun_13 = fun_12 ++ "4"
+
+fun_14 :: String
+fun_14 = "6" ++ "0" ++ fun_13
+
+fun_15 :: String
+fun_15 = fun_4 ++ fun_5 ++ fun_11 ++ fun_14
+
+fun_16 :: String
+fun_16 = fun_0 ++ "c" ++ "N" ++ fun_3 ++ fun_15
