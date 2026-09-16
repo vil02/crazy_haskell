dd086bc0b2900e5d71b17469300f820614869a3a
vil02 <vil02@users.noreply.github.com>
2026-09-16 00:48:22 +0000

    feat: add `p_630`

---
+++ src/p_630.hs
@@ -0,0 +1,54 @@
+import Prelude (IO, putStr, String, (++))
+main :: IO ()
+main = putStr fun_16
+
+fun_0 :: String
+fun_0 = "n" ++ "J" ++ "s"
+
+fun_1 :: String
+fun_1 = "y" ++ "8"
+
+fun_2 :: String
+fun_2 = "V" ++ "w"
+
+fun_3 :: String
+fun_3 = "T" ++ "1"
+
+fun_4 :: String
+fun_4 = "8" ++ fun_3
+
+fun_5 :: String
+fun_5 = "U" ++ "V"
+
+fun_6 :: String
+fun_6 = fun_5 ++ "e"
+
+fun_7 :: String
+fun_7 = "9" ++ "T"
+
+fun_8 :: String
+fun_8 = fun_4 ++ fun_6 ++ fun_7
+
+fun_9 :: String
+fun_9 = fun_8 ++ "A"
+
+fun_10 :: String
+fun_10 = fun_9 ++ "3" ++ "j"
+
+fun_11 :: String
+fun_11 = "D" ++ "f" ++ "a"
+
+fun_12 :: String
+fun_12 = "v" ++ "d" ++ "R"
+
+fun_13 :: String
+fun_13 = fun_12 ++ "V"
+
+fun_14 :: String
+fun_14 = fun_10 ++ fun_11 ++ fun_13
+
+fun_15 :: String
+fun_15 = "D" ++ fun_14 ++ "l"
+
+fun_16 :: String
+fun_16 = fun_0 ++ "r" ++ fun_1 ++ fun_2 ++ "5" ++ "J" ++ fun_15 ++ "t"
